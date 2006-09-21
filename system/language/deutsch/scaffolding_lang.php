cfc1fb38f0d7cd49c735730a603f7ca5a98a664d
admin <devnull@localhost>
2006-09-21 04:47:01 +0000

    Adding deutsch folder

---
+++ system/language/deutsch/scaffolding_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['scaff_view_records']		= 'Zeige Datens&auml;tze';
+$lang['scaff_create_record']	= 'Erstelle einen neuen Datensatz';
+$lang['scaff_add']				= 'Daten hinzuf&uuml;gen';
+$lang['scaff_view']				= 'Daten anzeigen';
+$lang['scaff_edit']				= 'Bearbeiten';
+$lang['scaff_delete']			= 'L&ouml;schen';
+$lang['scaff_view_all']			= 'Zeige alles';
+$lang['scaff_yes']				= 'Ja';
+$lang['scaff_no']				= 'Nein';
+$lang['scaff_no_data']			= 'Zur Zeit enth&auml;lt diese Tabelle noch keine Daten.';
+$lang['scaff_del_confirm']		= 'Sind Sie sicher, dass Sie die folgende Zeile l&ouml;schen m&ouml;chten:';
+
+?>
