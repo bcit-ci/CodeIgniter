37e2e202a3962f942cd86fb73b48fa3d001e722e
Derek Allard <derek.allard@ellislab.com>
2007-04-30 11:54:45 +0000

    fixed bug in output

--- system/libraries/Profiler.php
+++ system/libraries/Profiler.php
@@ -166,7 +166,7 @@ class CI_Profiler {
 				}
 			
 //				$output .= "<tr><td width='50%' style='color:#000;background-color:#ddd;'>&#36;_POST[".$key."]&nbsp;&nbsp;</td><td width='50%' style='color:#009900;font-weight:normal;background-color:#ddd;'>".htmlspecialchars(stripslashes($val))."</td></tr>\n";
-				$output .= "<tr><td width='50%' style='color:#000;background-color:#ddd;'>$_POST[".$key."]  </td><td width='50%' style='color:#009900;font-weight:normal;background-color:#ddd;'>";
+				$output .= "<tr><td width='50%' style='color:#000;background-color:#ddd;'>".$_POST[$key]." </td><td width='50%' style='color:#009900;font-weight:normal;background-color:#ddd;'>";
 				if (is_array($val))
 				{
 					$output .= "<pre>" . htmlspecialchars(stripslashes(print_r($val, true))) . "</pre>";
