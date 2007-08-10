428e96442ce4534e337404e5d4a436bd753b7cf6
Derek Allard <derek.allard@ellislab.com>
2007-08-10 03:16:16 +0000

    Added Flashdata variables, session_id regeneration and configurable session update times to the Session class

--- system/application/config/config.php
+++ system/application/config/config.php
@@ -227,6 +227,7 @@ $config['encryption_key'] = "";
 | 'encrypt_sess_cookie' = TRUE/FALSE (boolean).  Whether to encrypt the cookie
 | 'session_expiration'  = the number of SECONDS you want the session to last.
 |  by default sessions last 7200 seconds (two hours).  Set to zero for no expiration.
+| 'time_to_update'		= how many seconds between CI refreshing Session Information
 |
 */
 $config['sess_cookie_name']		= 'ci_session';
@@ -236,6 +237,7 @@ $config['sess_use_database']	= FALSE;
 $config['sess_table_name']		= 'ci_sessions';
 $config['sess_match_ip']		= FALSE;
 $config['sess_match_useragent']	= TRUE;
+$config['sess_time_to_update'] 		= 300;
 
 /*
 |--------------------------------------------------------------------------
