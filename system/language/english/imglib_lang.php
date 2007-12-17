488b7ee038ceca4ed083a1fd4cc43788e20101a7
Derek Allard <derek.allard@ellislab.com>
2007-12-17 14:22:31 +0000

    Fixed a typo in the image language file "suppor" to "support".

--- system/language/english/imglib_lang.php
+++ system/language/english/imglib_lang.php
@@ -2,7 +2,7 @@
 
 $lang['imglib_source_image_required'] = "You must specify a source image in your preferences.";
 $lang['imglib_gd_required'] = "The GD image library is required for this feature.";
-$lang['imglib_gd_required_for_props'] = "Your server must suppor the GD image library in order to determine the image properties";
+$lang['imglib_gd_required_for_props'] = "Your server must support the GD image library in order to determine the image properties";
 $lang['imglib_unsupported_imagecreate'] = "Your server does not support the GD function required to process this type of image.";
 $lang['imglib_gif_not_supported'] = "GIF images are often not supported due to licensing restrictions.  You may have to use JPG or PNG images instead.";
 $lang['imglib_jpg_not_supported'] = "JPG images are not supported";
