8a84a8a681718ce4d5e3201439b8c0c2fce89592
Derek Allard <derek.allard@ellislab.com>
2008-01-10 18:00:13 +0000

    added support for external urls in form_open

--- system/helpers/form_helper.php
+++ system/helpers/form_helper.php
@@ -42,7 +42,9 @@ function form_open($action = '', $attributes = array(), $hidden = array())
 {
 	$CI =& get_instance();
 
-	$form = '<form action="'.$CI->config->site_url($action).'"';
+	$action = ( strpos($action, '://') === FALSE) ? $CI->config->site_url($action) : $action;
+
+	$form = '<form action="'.$action.'"';
 	
 	if ( ! isset($attributes['method']))
 	{
@@ -229,7 +231,7 @@ function form_dropdown($name = '', $options = array(), $selected = array(), $ext
 	}
 
 	if ($extra != '') $extra = ' '.$extra;
-	$multiple = (count($selected) > 1)?' multiple="multiple"':'';
+	$multiple = (count($selected > 1))?' multiple="multiple"':'';
 
 	$form = '<select name="'.$name.'"'.$extra.$multiple.">\n";
 	
