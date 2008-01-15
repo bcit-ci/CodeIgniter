ac05e06d3c5f0666505e3f0aabe9be8f485f436c
Derek Allard <derek.allard@ellislab.com>
2008-01-15 13:58:35 +0000

    Fixed typos in the email Language class (email_attachment_unredable, email_filed_smtp_login).

--- system/language/english/email_lang.php
+++ system/language/english/email_lang.php
@@ -3,7 +3,7 @@
 $lang['email_must_be_array'] = "The email validation method must be passed an array.";
 $lang['email_invalid_address'] = "Invalid email address: %s";
 $lang['email_attachment_missing'] = "Unable to locate the following email attachment: %s";
-$lang['email_attachment_unredable'] = "Unable to open this attachment: %s";
+$lang['email_attachment_unreadable'] = "Unable to open this attachment: %s";
 $lang['email_no_recipients'] = "You must include recipients: To, Cc, or Bcc";
 $lang['email_send_failure_phpmail'] = "Unable to send email using PHP mail().  Your server might not be configured to send mail using this method.";
 $lang['email_send_failure_sendmail'] = "Unable to send email using PHP Sendmail.  Your server might not be configured to send mail using this method.";
@@ -13,7 +13,7 @@ $lang['email_no_socket'] = "Unable to open a socket to Sendmail. Please check se
 $lang['email_no_hostname'] = "You did not specify a SMTP hostname";
 $lang['email_smtp_error'] = "The following SMTP error was encountered: %s";
 $lang['email_no_smtp_unpw'] = "Error: You must assign a SMTP username and password.";
-$lang['email_filed_smtp_login'] = "Failed to send AUTH LOGIN command. Error: %s";
+$lang['email_failed_smtp_login'] = "Failed to send AUTH LOGIN command. Error: %s";
 $lang['email_smtp_auth_un'] = "Failed to authenticate username. Error: %s";
 $lang['email_smtp_auth_pw'] = "Failed to authenticate password. Error: %s";
 $lang['email_smtp_data_failure'] = "Unable to send data: %s";
