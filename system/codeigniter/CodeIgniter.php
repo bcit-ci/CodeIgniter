f30445be8871f71bca9a4b11fa4e49edff0ac936
Derek Jones <derek.jones@ellislab.com>
2008-01-30 05:23:32 +0000

    Fixed a bug (#2849) with arguments passed to a subfolder controller - CodeIgniter.php was expecting the subfolder to still be in $URI->rsegments, but it is removed already by Router::_validate_request()

--- system/codeigniter/CodeIgniter.php
+++ system/codeigniter/CodeIgniter.php
@@ -221,7 +221,7 @@ else
 
 		// Call the requested method.
 		// Any URI segments present (besides the class/function) will be passed to the method for convenience		
-		call_user_func_array(array(&$CI, $method), array_slice($URI->rsegments, (($RTR->fetch_directory() == '') ? 2 : 3)));		
+		call_user_func_array(array(&$CI, $method), array_slice($URI->rsegments, 2));		
 	}
 }
 
