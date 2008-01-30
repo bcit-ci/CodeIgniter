afc7fd52389adf43756e1083fe854ce0da61710f
Derek Jones <derek.jones@ellislab.com>
2008-01-30 20:28:55 +0000

    actually removed 'active_r' from database.php config like I said I already did...

--- system/application/config/database.php
+++ system/application/config/database.php
@@ -43,7 +43,6 @@ $db['default']['password'] = "";
 $db['default']['database'] = "";
 $db['default']['dbdriver'] = "mysql";
 $db['default']['dbprefix'] = "";
-$db['default']['active_r'] = TRUE;
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
 $db['default']['cache_on'] = FALSE;
