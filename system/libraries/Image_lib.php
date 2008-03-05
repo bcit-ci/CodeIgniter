3e4f65885a0efdda1d9708a5737d61f2d8056ef5
Derek Allard <derek.allard@ellislab.com>
2008-03-05 16:22:31 +0000

    restore a comment

--- system/libraries/Image_lib.php
+++ system/libraries/Image_lib.php
@@ -499,8 +499,14 @@ class CI_Image_lib {
 			return FALSE;
 		}
 
-		//  Create The Image
-		if ($this->image_library == 'gd2' AND function_exists('imagecreatetruecolor'))
+ 		//  Create The Image
+		//
+		//  old conditional which users report cause problems with shared GD libs who report themselves as "2.0 or greater"
+		//  it appears that this is no longer the issue that it was in 2004, so we've removed it, retaining it in the comment
+		//  below should that ever prove inaccurate.
+		//
+		//  if ($this->image_library == 'gd2' AND function_exists('imagecreatetruecolor') AND $v2_override == FALSE)
+ 		if ($this->image_library == 'gd2' AND function_exists('imagecreatetruecolor'))		
 		{
 			$create	= 'imagecreatetruecolor';
 			$copy	= 'imagecopyresampled';
