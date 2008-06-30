e37fa05e9687ff461b1ff7413633ea697365c128
Derek Jones <derek.jones@ellislab.com>
2008-06-30 00:22:42 +0000

    fixed some whitespace in the number helper and improved on the calculation method

--- system/helpers/number_helper.php
+++ system/helpers/number_helper.php
@@ -40,26 +40,26 @@ if ( ! function_exists('byte_format'))
 	{
 		$CI =& get_instance();
 		$CI->lang->load('number');
-
+	
 		if ($num >= 1000000000000) 
 		{
-			$num = round($num/1099511627776)/10;
-			$unit  = $CI->lang->line('terabyte_abbr');
+			$num = round($num / 1099511627776, 1);
+			$unit = $CI->lang->line('terabyte_abbr');
 		}
 		elseif ($num >= 1000000000) 
 		{
-			$num = round($num/107374182)/10;
-			$unit  = $CI->lang->line('gigabyte_abbr');
+			$num = round($num / 1073741824, 1);
+			$unit = $CI->lang->line('gigabyte_abbr');
 		}
 		elseif ($num >= 1000000) 
 		{
-			$num = round($num/104857)/10;
-			$unit  = $CI->lang->line('megabyte_abbr');
+			$num = round($num / 1048576, 1);
+			$unit = $CI->lang->line('megabyte_abbr');
 		}
 		elseif ($num >= 1000) 
 		{
-			$num = round($num/102)/10;
-			$unit  = $CI->lang->line('kilobyte_abbr');
+			$num = round($num / 1024, 1);
+			$unit = $CI->lang->line('kilobyte_abbr');
 		}
 		else
 		{
