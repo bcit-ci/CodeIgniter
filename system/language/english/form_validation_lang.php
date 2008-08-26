ff8f0c48047560b949a39be7322568a25475ab64
Rick Ellis <rick.ellis@ellislab.com>
2008-08-26 19:48:08 +0000

    Added Form Validation Library and updated docs

---
+++ system/language/english/form_validation_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['required'] 			= "The %s field is required.";
+$lang['isset']				= "The %s field must have a value.";
+$lang['valid_email']		= "The %s field must contain a valid email address.";
+$lang['valid_emails'] 		= "The %s field must contain all valid email addresses.";
+$lang['valid_url'] 			= "The %s field must contain a valid URL.";
+$lang['valid_ip'] 			= "The %s field must contain a valid IP.";
+$lang['min_length']			= "The %s field must be at least %s characters in length.";
+$lang['max_length']			= "The %s field can not exceed %s characters in length.";
+$lang['exact_length']		= "The %s field must be exactly %s characters in length.";
+$lang['alpha']				= "The %s field may only contain alphabetical characters.";
+$lang['alpha_numeric']		= "The %s field may only contain alpha-numeric characters.";
+$lang['alpha_dash']			= "The %s field may only contain alpha-numeric characters, underscores, and dashes.";
+$lang['numeric']			= "The %s field must contain a number.";
+$lang['is_numeric']			= "The %s field must contain a number.";
+$lang['integer']			= "The %s field must contain an integer.";
+$lang['matches']			= "The %s field does not match the %s field.";
+$lang['is_natural']			= "The %s field must contain a number.";
+$lang['is_natural_no_zero']	= "The %s field must contain a number greater than zero.";
+
+
+/* End of file validation_lang.php */
+/* Location: ./system/language/english/validation_lang.php */
