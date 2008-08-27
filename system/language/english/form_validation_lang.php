51d3c2d75c5b887f50371365366912c6b79d885f
Rick Ellis <rick.ellis@ellislab.com>
2008-08-27 21:46:24 +0000

    Clarified some of the lines of text

--- system/language/english/form_validation_lang.php
+++ system/language/english/form_validation_lang.php
@@ -12,11 +12,11 @@ $lang['exact_length']		= "The %s field must be exactly %s characters in length."
 $lang['alpha']				= "The %s field may only contain alphabetical characters.";
 $lang['alpha_numeric']		= "The %s field may only contain alpha-numeric characters.";
 $lang['alpha_dash']			= "The %s field may only contain alpha-numeric characters, underscores, and dashes.";
-$lang['numeric']			= "The %s field must contain a number.";
-$lang['is_numeric']			= "The %s field must contain a number.";
+$lang['numeric']			= "The %s field must contain only numbers.";
+$lang['is_numeric']			= "The %s field must contain only numeric characters.";
 $lang['integer']			= "The %s field must contain an integer.";
 $lang['matches']			= "The %s field does not match the %s field.";
-$lang['is_natural']			= "The %s field must contain a number.";
+$lang['is_natural']			= "The %s field must contain only positive numbers.";
 $lang['is_natural_no_zero']	= "The %s field must contain a number greater than zero.";
 
 
