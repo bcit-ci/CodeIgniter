d81eb9513bd983351024f1719455a32c5328bfb0
Rick Ellis <rick.ellis@ellislab.com>
2008-09-21 19:33:43 +0000

    Added line of text for new profiler block

--- system/language/english/profiler_lang.php
+++ system/language/english/profiler_lang.php
@@ -1,5 +1,6 @@
 <?php
 
+$lang['profiler_controller_info'] = 'CLASS/METHOD';
 $lang['profiler_benchmarks']	= 'BENCHMARKS';
 $lang['profiler_queries']		= 'QUERIES';
 $lang['profiler_get_data']		= 'GET DATA';
