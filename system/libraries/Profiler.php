46329613317adbe740074023e02a5c37dbe63c38
Derek Allard <derek.allard@ellislab.com>
2008-10-17 15:42:32 +0000

    re-implementing profiler output

--- system/libraries/Profiler.php
+++ system/libraries/Profiler.php
@@ -364,20 +364,19 @@ class CI_Profiler {
 	 * @return	string
 	 */	
 	function run()
-	{		
-		$output = '<br clear="all" />';
-		$output .= "<div style='background-color:#fff;padding:10px;'>";
+	{
+		$output .= "<div id='codeigniter_profiler' style='clear:both;background-color:#fff;padding:10px;'>";
 
 		$output .= $this->_compile_uri_string();
 		$output .= $this->_compile_controller_info();
 		$output .= $this->_compile_memory_usage();
-		$output .= $this->_compile_benchmarks();	
+		$output .= $this->_compile_benchmarks();
 		$output .= $this->_compile_get();
 		$output .= $this->_compile_post();
 		$output .= $this->_compile_queries();
-		
+
 		$output .= '</div>';
-		
+
 		return $output;
 	}
 
