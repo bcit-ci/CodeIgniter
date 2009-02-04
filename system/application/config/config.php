abb0adb40486976f137b4d4f42dda5113926a54d
Derek Jones <derek.jones@ellislab.com>
2009-02-04 21:44:40 +0000

    changed your-site.com to example.com

--- system/application/config/config.php
+++ system/application/config/config.php
@@ -11,7 +11,7 @@
 |	http://example.com/
 |
 */
-$config['base_url']	= "http://your-site.com/";
+$config['base_url']	= "http://example.com/";
 
 /*
 |--------------------------------------------------------------------------
