16bb491f8b9b6921bd674732be521f6c692c768d
Derek Jones <derek.jones@ellislab.com>
2009-02-10 17:11:52 +0000

    added sanity check for images in is_allowed_filetype() using getimagesize()

--- system/libraries/Upload.php
+++ system/libraries/Upload.php
@@ -556,11 +556,22 @@ class CI_Upload {
 			$this->set_error('upload_no_file_types');
 			return FALSE;
 		}
-			 	
+
+		$image_types = array('gif', 'jpg', 'jpeg', 'png', 'jpe');
+
 		foreach ($this->allowed_types as $val)
 		{
 			$mime = $this->mimes_types(strtolower($val));
-		
+
+			// Images get some additional checks
+			if (in_array($val, $image_types))
+			{
+				if (getimagesize($this->file_temp) === FALSE)
+				{
+					return FALSE;
+				}
+			}
+
 			if (is_array($mime))
 			{
 				if (in_array($this->file_type, $mime, TRUE))
