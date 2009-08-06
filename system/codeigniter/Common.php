77b513b821bf6df4b3afd4b841db93999632050e
Derek Jones <derek.jones@ellislab.com>
2009-08-06 14:39:25 +0000

    fixed a bug in r1718 where the PHP version key was cast as float in a particular test, making the array keys used by is_php() inaccurate beyond whole number versions
    http://codeigniter.com/bug_tracker/bug/8475/

--- system/codeigniter/Common.php
+++ system/codeigniter/Common.php
@@ -36,12 +36,14 @@
 * we'll set a static variable.
 *
 * @access	public
-* @return	bool
+* @param	string
+* * @return	bool
 */
 function is_php($version = '5.0.0')
 {
 	static $_is_php;
-
+	$version = (string)$version;
+	
 	if ( ! isset($_is_php[$version]))
 	{
 		$_is_php[$version] = (version_compare(PHP_VERSION, $version) < 0) ? FALSE : TRUE;
