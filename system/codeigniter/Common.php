7b6b3f66a72530481c20587d17b9cf85976d7327
Derek Jones <derek.jones@ellislab.com>
2009-09-24 14:25:39 +0000

    increased randomness of file name used in is_really_writable()

--- system/codeigniter/Common.php
+++ system/codeigniter/Common.php
@@ -62,7 +62,7 @@ function is_really_writable($file)
 	// write a file then read it.  Bah...
 	if (is_dir($file))
 	{
-		$file = rtrim($file, '/').'/'.md5(rand(1,100));
+		$file = rtrim($file, '/').'/'.md5(mt_rand(1,100).mt_rand(1,100));
 
 		if (($fp = @fopen($file, FOPEN_WRITE_CREATE)) === FALSE)
 		{
