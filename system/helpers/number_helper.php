ed6103c52aee58fd6609944b5842f9723a793a7b
Derek Allard <derek.allard@ellislab.com>
2010-01-17 16:13:15 +0000

    optional precision argument in byte_format()

--- system/helpers/number_helper.php
+++ system/helpers/number_helper.php
@@ -36,29 +36,29 @@
  */
 if ( ! function_exists('byte_format'))
 {
-	function byte_format($num)
+	function byte_format($num, $precision = 1)
 	{
 		$CI =& get_instance();
 		$CI->lang->load('number');
 	
 		if ($num >= 1000000000000) 
 		{
-			$num = round($num / 1099511627776, 1);
+			$num = round($num / 1099511627776, $precision);
 			$unit = $CI->lang->line('terabyte_abbr');
 		}
 		elseif ($num >= 1000000000) 
 		{
-			$num = round($num / 1073741824, 1);
+			$num = round($num / 1073741824, $precision);
 			$unit = $CI->lang->line('gigabyte_abbr');
 		}
 		elseif ($num >= 1000000) 
 		{
-			$num = round($num / 1048576, 1);
+			$num = round($num / 1048576, $precision);
 			$unit = $CI->lang->line('megabyte_abbr');
 		}
 		elseif ($num >= 1000) 
 		{
-			$num = round($num / 1024, 1);
+			$num = round($num / 1024, $precision);
 			$unit = $CI->lang->line('kilobyte_abbr');
 		}
 		else
@@ -67,9 +67,11 @@ if ( ! function_exists('byte_format'))
 			return number_format($num).' '.$unit;
 		}
 
-		return number_format($num, 1).' '.$unit;
+		return number_format($num, $precision).' '.$unit;
 	}	
 }
+
+
 
 /* End of file number_helper.php */
 /* Location: ./system/helpers/number_helper.php */
