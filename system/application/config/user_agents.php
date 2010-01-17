9ae73826bfdab0d9651d5633c493b94f223332f6
Derek Allard <derek.allard@ellislab.com>
2010-01-17 16:02:01 +0000

    Added Flock to the list of browsers in the user agent class

--- system/application/config/user_agents.php
+++ system/application/config/user_agents.php
@@ -48,6 +48,7 @@ $platforms = array (
 // The order of this array should NOT be changed. Many browsers return
 // multiple browser types so we want to identify the sub-type first.
 $browsers = array(
+					'Flock'				=> 'Flock',
 					'Chrome'			=> 'Chrome',
 					'Opera'				=> 'Opera',
 					'MSIE'				=> 'Internet Explorer',
