2a58e7a6945c47929d5003f15f1bdc32a1717270
Derek Jones <derek.jones@ellislab.com>
2010-03-02 17:53:38 -0600

    bringing language files up to date

--- system/language/english/date_lang.php
+++ system/language/english/date_lang.php
@@ -56,5 +56,6 @@ $lang['UP1275']	= '(UTC +12:45) Chatham Islands Standard Time';
 $lang['UP13']	= '(UTC +13:00) Phoenix Islands Time, Tonga';
 $lang['UP14']	= '(UTC +14:00) Line Islands';
 
+
 /* End of file date_lang.php */
 /* Location: ./system/language/english/date_lang.php */
