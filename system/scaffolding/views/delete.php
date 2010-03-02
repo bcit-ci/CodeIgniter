cf24ffea2b91f48900c3c5a19ce11982f76c45ed
Derek Jones <derek.jones@ellislab.com>
2010-03-02 15:37:56 -0600

    dumping scaffolding folder

--- system/scaffolding/views/delete.php
+++
@@ -1,9 +0,0 @@
-<?php  $this->load->view('header');  ?>
-
-<p><?php echo $message; ?></p>
-
-<p><?php echo $no; ?>&nbsp;&nbsp;|&nbsp;&nbsp;<?php echo $yes; ?>
-
-<?php $this->load->view('footer'); 
-/* End of file delete.php */
-/* Location: ./system/scaffolding/views/delete.php */
--- system/scaffolding/views/no_data.php
+++
@@ -1,8 +0,0 @@
-<?php  $this->load->view('header');  ?>
-
-<p><?php echo $scaff_no_data; ?></p>
-<p><?php echo anchor(array($base_uri, 'add'), $scaff_create_record); ?></p>
-
-<?php $this->load->view('footer'); 
-/* End of file no_data.php */
-/* Location: ./system/scaffolding/views/no_data.php */
