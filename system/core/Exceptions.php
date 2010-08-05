2ddc9496e9403a59a87b644d1c2b9a106b773e46
Derek Allard <derek.allard@ellislab.com>
2010-08-05 10:08:33 -0400

    Added an optional second parameter to <kbd>show_404()</kbd> to disable logging.

--- system/core/Exceptions.php
+++ system/core/Exceptions.php
@@ -88,12 +88,17 @@ class CI_Exceptions {
 	 * @param	string
 	 * @return	string
 	 */
-	function show_404($page = '')
+	function show_404($page = '', $log_error = TRUE)
 	{	
 		$heading = "404 Page Not Found";
 		$message = "The page you requested was not found.";
 
-		log_message('error', '404 Page Not Found --> '.$page);
+		// By default we log this, but allow a dev to skip it
+		if ($log_error)
+		{
+			log_message('error', '404 Page Not Found --> '.$page);
+		}
+
 		echo $this->show_error($heading, $message, 'error_404', 404);
 		exit;
 	}
