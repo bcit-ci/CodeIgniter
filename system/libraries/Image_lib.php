595bfd1484ecc8212d6c3c028210b4d1ae78baba
Derek Jones <derek.jones@ellislab.com>
2010-08-20 10:28:22 -0500

    Added ability in the Image Library to handle PNG transparency for resize operations when using the GD lib.

--- system/libraries/Image_lib.php
+++ system/libraries/Image_lib.php
@@ -513,6 +513,13 @@ class CI_Image_lib {
 		}
 
 		$dst_img = $create($this->width, $this->height);
+
+		if ($this->image_type == 3) // png we can actually preserve transparency
+		{
+			imagealphablending($dst_img, FALSE);
+			imagesavealpha($dst_img, TRUE);
+		}
+		
 		$copy($dst_img, $src_img, 0, 0, $this->x_axis, $this->y_axis, $this->width, $this->height, $this->orig_width, $this->orig_height);
 
 		//  Show the image
