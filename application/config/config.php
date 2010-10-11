c288ea957dd5b01abba96c9644f309a2714482e7
Greg Aker <greg.aker@ellislab.com>
2010-10-11 15:41:19 -0500

    Fix #120  -- Wording of Cross Site Request Forgery in config.php

--- application/config/config.php
+++ application/config/config.php
@@ -274,7 +274,7 @@ $config['global_xss_filtering'] = FALSE;
 
 /*
 |--------------------------------------------------------------------------
-| Cross Site Forgery Request
+| Cross Site Request Forgery
 |--------------------------------------------------------------------------
 | Enables a CSFR cookie token to be set. When set to TRUE, token will be
 | checked on a submitted form. If you are accepting user data, it is strongly
