8af74554043be56daa9fb278e60ee79a53be4b76
Eric Barnes <eric@ericlbarnes.com>
2011-01-16 23:36:46 -0500

    Accidentally left csrf to turned on in last commit

--- application/config/config.php
+++ application/config/config.php
@@ -291,7 +291,7 @@ $config['global_xss_filtering'] = FALSE;
 | 'csrf_cookie_name' = The cookie name
 | 'csrf_expire' = The number in seconds the token should expire.
 */
-$config['csrf_protection'] = TRUE;
+$config['csrf_protection'] = FALSE;
 $config['csrf_token_name'] = 'csrf_test_name';
 $config['csrf_cookie_name'] = 'csrf_cookie_name';
 $config['csrf_expire'] = 7200;
