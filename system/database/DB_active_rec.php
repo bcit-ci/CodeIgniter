d0ac1a250608c1fe21f11bb96c4291e38962b187
Phil Sturgeon <email@philsturgeon.co.uk>
2011-02-15 22:54:08 +0000

    Better potential fix for escaping MySQL keywords with backticks on insert/update.

--- system/database/DB_active_rec.php
+++ system/database/DB_active_rec.php
@@ -954,7 +954,7 @@ class CI_DB_active_record extends CI_DB_driver {
 			}
 			else
 			{
-				$this->ar_set[$this->_protect_identifiers($k)] = $this->escape($v);
+				$this->ar_set[$this->_protect_identifiers($k, FALSE, TRUE)] = $this->escape($v);
 			}
 		}
 
@@ -1156,7 +1156,7 @@ class CI_DB_active_record extends CI_DB_driver {
 				$this->ar_set[] = array();
 				return;
 			}
-		
+
 			ksort($row); // puts $row in the same order as our keys
 
 			if ($escape === FALSE)
