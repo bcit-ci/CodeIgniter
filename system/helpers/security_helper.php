6b488674368cf695a228e87e7d8e0f4f40fe4181
Pascal Kriete <pascal.kriete@ellislab.com>
2011-04-05 15:02:15 -0400

    Removing dohash and deprecating CI_SHA

--- system/helpers/security_helper.php
+++ system/helpers/security_helper.php
@@ -67,22 +67,6 @@ if ( ! function_exists('sanitize_filename'))
 /**
  * Hash encode a string
  *
- * This is simply an alias for do_hash()
- * dohash() is now deprecated
- */
-if ( ! function_exists('dohash'))
-{
-	function dohash($str, $type = 'sha1')
-	{
-		return do_hash($str, $type);
-	}
-}
-
-// --------------------------------------------------------------------
-
-/**
- * Hash encode a string
- *
  * @access	public
  * @param	string
  * @return	string
@@ -93,23 +77,7 @@ if ( ! function_exists('do_hash'))
 	{
 		if ($type == 'sha1')
 		{
-			if ( ! function_exists('sha1'))
-			{
-				if ( ! function_exists('mhash'))
-				{
-					require_once(BASEPATH.'libraries/Sha1'.EXT);
-					$SH = new CI_SHA;
-					return $SH->generate($str);
-				}
-				else
-				{
-					return bin2hex(mhash(MHASH_SHA1, $str));
-				}
-			}
-			else
-			{
-				return sha1($str);
-			}
+			return sha1($str);
 		}
 		else
 		{
