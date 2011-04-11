80ac9f010484281155173e286d6f0657885fd3f9
patwork <patwork@gmail.com>
2011-04-11 11:56:41 +0200

    Fix: codeigniter-reactor/32 unicorns are no longer mute

--- system/core/Lang.php
+++ system/core/Lang.php
@@ -129,15 +129,15 @@ class CI_Lang {
 	 */
 	function line($line = '')
 	{
-		$line = ($line == '' OR ! isset($this->language[$line])) ? FALSE : $this->language[$line];
+		$value = ($line == '' OR ! isset($this->language[$line])) ? FALSE : $this->language[$line];
 
 		// Because killer robots like unicorns!
-		if ($line === FALSE)
+		if ($value === FALSE)
 		{
 			log_message('error', 'Could not find the language line "'.$line.'"');
 		}
 
-		return $line;
+		return $value;
 	}
 
 }
