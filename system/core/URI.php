c03865c7a94b7282d3998fbfa10173d2e98f7448
Eric Barnes <eric@ericlbarnes.com>
2011-04-17 23:45:41 -0400

    Changed server check to ensure SCRIPT_NAME is defined. Fixes #57

--- system/core/URI.php
+++ system/core/URI.php
@@ -120,7 +120,7 @@ class CI_URI {
 		$path = (isset($_SERVER[$uri])) ? $_SERVER[$uri] : @getenv($uri);
 		$this->_set_uri_string($path);
 	}
-	
+
 	// --------------------------------------------------------------------
 
 	/**
@@ -133,7 +133,7 @@ class CI_URI {
 	{
 		// Filter out control characters
 		$str = remove_invisible_characters($str, FALSE);
-		
+
 		// If the URI contains only a slash we'll kill it
 		$this->uri_string = ($str == '/') ? '' : $str;
 	}
@@ -151,7 +151,7 @@ class CI_URI {
 	 */
 	private function _detect_uri()
 	{
-		if ( ! isset($_SERVER['REQUEST_URI']))
+		if ( ! isset($_SERVER['REQUEST_URI']) OR ! isset($_SERVER['SCRIPT_NAME']))
 		{
 			return '';
 		}
@@ -184,12 +184,12 @@ class CI_URI {
 			$_SERVER['QUERY_STRING'] = '';
 			$_GET = array();
 		}
-		
+
 		if ($uri == '/' || empty($uri))
 		{
 			return '/';
 		}
-				
+
 		$uri = parse_url($uri, PHP_URL_PATH);
 
 		// Do some final cleaning of the URI and return it
