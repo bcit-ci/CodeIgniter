b567947d38a20960aade96f9179ad6bc21f22646
Greg Aker <greg.aker@ellislab.com>
2011-04-21 14:34:31 -0500

    Moving tests/codeigniter/helpers/Array_helper_test.php to be lower cased.
    Adding html_helper_test.php as well.

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -17,4 +17,12 @@ define('APPPATH',		PROJECT_BASE.'application/');
 require_once $dir.'/lib/common.php';
 require_once $dir.'/lib/ci_testcase.php';
 
+
+// Omit files in the PEAR & PHP Paths from ending up in the coverage report
+PHP_CodeCoverage_Filter::getInstance()->addDirectoryToBlacklist(PEAR_INSTALL_DIR);	
+PHP_CodeCoverage_Filter::getInstance()->addDirectoryToBlacklist(PHP_LIBDIR);	
+
+// Omit Tests from the coverage reports.
+// PHP_CodeCoverage_Filter::getInstance()
+
 unset($dir);
