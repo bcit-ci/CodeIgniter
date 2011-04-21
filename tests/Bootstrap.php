f5aee9d1532b175d2dbfbb66f56e8861f34cd861
Pascal Kriete <pascal.kriete@ellislab.com>
2011-04-21 01:20:40 -0400

    some basic bootstrap cleanup

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,13 +1,20 @@
 <?php
 
+// Errors on full!
 ini_set('display_errors', 1);
 error_reporting(E_ALL | E_STRICT);
 
-define('PROJECT_BASE',	realpath(dirname(__FILE__).'/../').'/');
+$dir = realpath(dirname(__FILE__));
 
+
+// Path constants
+define('PROJECT_BASE',	realpath($dir.'/../').'/');
 define('BASEPATH',		PROJECT_BASE.'system/');
 define('APPPATH',		PROJECT_BASE.'application/');
 
-// set up a highly controlled CI environment
-require_once './lib/common.php';
-require_once './lib/ci_testcase.php';
+
+// Prep our test environment
+require_once $dir.'/lib/common.php';
+require_once $dir.'/lib/ci_testcase.php';
+
+unset($dir);
