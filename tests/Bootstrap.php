d0e3610dc6bba54a364622ecadf0569a4a5945a5
Pascal Kriete <pascal.kriete@ellislab.com>
2011-04-21 00:58:51 -0400

    Removing some lefover code form experimenting with process isolation.

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -3,18 +3,10 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL | E_STRICT);
 
-if ( ! defined('PROJECT_BASE'))
-{
-	define('PROJECT_BASE',	realpath(dirname(__FILE__).'/../').'/');
+define('PROJECT_BASE',	realpath(dirname(__FILE__).'/../').'/');
 
-	define('BASEPATH',		PROJECT_BASE.'system/');
-	define('APPPATH',		PROJECT_BASE.'application/');
-}
-// define('EXT', '.php');
-
-// @todo provide a way to set various config options
-
-
+define('BASEPATH',		PROJECT_BASE.'system/');
+define('APPPATH',		PROJECT_BASE.'application/');
 
 // set up a highly controlled CI environment
 require_once './lib/common.php';
