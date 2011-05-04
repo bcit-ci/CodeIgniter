96bd33b2edc1b0e6a04cb8e3bcf97e8c7b3adf3e
Phil Sturgeon <email@philsturgeon.co.uk>
2011-05-04 01:30:36 +0100

    Added Migration class and language file.

--- application/config/migration.php
+++ application/config/migration.php
@@ -22,7 +22,7 @@ $config['migration_enabled'] = TRUE;
 | be upgraded / downgraded to.
 |
 */
-$config['migration_version'] = 1;
+$config['migration_version'] = 0;
 
 
 /*
@@ -36,3 +36,7 @@ $config['migration_version'] = 1;
 |
 */
 $config['migration_path'] = APPPATH . 'migrations/';
+
+
+/* End of file migration.php */
+/* Location: ./application/config/migration.php */
