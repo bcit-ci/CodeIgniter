a8cb5808979e1faab66cf69b2af2d7af5dadb2bd
Phil Sturgeon <email@philsturgeon.co.uk>
2011-08-10 08:06:37 -0600

    Fixed a bug where Email library attachments with a . in the name would using invalid MIME-types.

--- system/libraries/Email.php
+++ system/libraries/Email.php
@@ -395,7 +395,7 @@ class CI_Email {
 	public function attach($filename, $disposition = 'attachment')
 	{
 		$this->_attach_name[] = $filename;
-		$this->_attach_type[] = $this->_mime_types(next(explode('.', basename($filename))));
+		$this->_attach_type[] = $this->_mime_types(pathinfo($filename, PATHINFO_EXTENSION));
 		$this->_attach_disp[] = $disposition; // Can also be 'inline'  Not sure if it matters
 		return $this;
 	}
