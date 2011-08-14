47e59709adc81f2225c5635501eeab628902c662
Phil Sturgeon <email@philsturgeon.co.uk>
2011-08-14 12:23:55 -0600

    Added increment_string() docs and changelog.

--- system/helpers/string_helper.php
+++ system/helpers/string_helper.php
@@ -246,9 +246,11 @@ if ( ! function_exists('random_string'))
  * Add's _1 to a string or increment the ending number to allow _2, _3, etc
  *
  * @param   string  $str  required
+ * @param   string  $separator  What should the duplicate number be appended with
+ * @param   string  $first  Which number should be used for the first dupe increment
  * @return  string
  */
-function increment_string($str, $first = 1, $separator = '_')
+function increment_string($str, $separator = '_', $first = 1)
 {
 	preg_match('/(.+)'.$separator.'([0-9]+)$/', $str, $match);
 
