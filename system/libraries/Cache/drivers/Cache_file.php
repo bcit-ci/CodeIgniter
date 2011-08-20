80f4c34d1c5a51d82ae3dd898a12466e01790f10
Ben Edmunds <ben.edmunds@gmail.com>
2011-08-20 14:11:21 -0500

    Resolved issue 160.  Removed unneeded array copy.

--- system/libraries/Cache/drivers/Cache_file.php
+++ system/libraries/Cache/drivers/Cache_file.php
@@ -157,16 +157,15 @@ class CI_Cache_file extends CI_Driver {
 		
 		if (is_array($data))
 		{
-			$data = $data['data'];
 			$mtime = filemtime($this->_cache_path.$id);
 
-			if ( ! isset($data['ttl']))
+			if ( ! isset($data['data']['ttl']))
 			{
 				return FALSE;
 			}
 
 			return array(
-				'expire' 	=> $mtime + $data['ttl'],
+				'expire' 	=> $mtime + $data['data']['ttl'],
 				'mtime'		=> $mtime
 			);
 		}
