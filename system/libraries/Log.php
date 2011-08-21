7164ea1a3b51c553fc8ef43dd0613c24b3c3c976
John Bellone <jb@thunkbrightly.com>
2011-08-21 16:18:57 -0400

    Once again being stylish.

--- system/libraries/Log.php
+++ system/libraries/Log.php
@@ -99,16 +99,16 @@ class CI_Log {
 		$message .= $level.' '.(($level == 'INFO') ? ' -' : '-').' '.date($this->_date_fmt). ' --> '.$msg."\n";
 
 		if (flock($fp, LOCK_EX | LOCK_NB))
-                {
-        		fwrite($fp, $message);
-        		flock($fp, LOCK_UN);
-                }
-                else
-                {
-        		fwrite($fp, $message);
-                }
+		{
+			fwrite($fp, $message);
+			flock($fp, LOCK_UN);
+		}
+		else
+		{
+			fwrite($fp, $message);
+		}
 
-       		fclose($fp);
+		fclose($fp);
 
 		@chmod($filepath, FILE_WRITE_MODE);
 		return TRUE;
