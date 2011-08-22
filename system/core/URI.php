398c22bb71e2867ae98605427e713f5a3639f990
Aleksandr Sytar <admin@vsedlyauborki.ru>
2011-08-22 13:14:34 +0400

    Add unicode-based check to URI

--- system/core/URI.php
+++ system/core/URI.php
@@ -254,7 +254,7 @@ class CI_URI {
 		{
 			// preg_quote() in PHP 5.3 escapes -, so the str_replace() and addition of - to preg_quote() is to maintain backwards
 			// compatibility as many are unaware of how characters in the permitted_uri_chars will be parsed as a regex pattern
-			if ( ! preg_match("|^[".str_replace(array('\\-', '\-'), '-', preg_quote($this->config->item('permitted_uri_chars'), '-'))."]+$|i", $str))
+			if ( ! preg_match("|^[".str_replace(array('\\-', '\-'), '-', preg_quote($this->config->item('permitted_uri_chars'), '-'))."]+$|ui", $str))
 			{
 				show_error('The URI you submitted has disallowed characters.', 400);
 			}
