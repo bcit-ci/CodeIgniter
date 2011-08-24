f09b2c713c4fe01b8ffad346f5aee669ed872eac
genio <cwhitener@gmail.com>
2011-08-23 22:58:21 -0300

    Took Phil's advice and got rid of the silly IF block.  The previous change got rid of the ternary operator in there, but I should have just gotten rid of the condition all together.

--- system/libraries/Form_validation.php
+++ system/libraries/Form_validation.php
@@ -1048,11 +1048,7 @@ class CI_Form_validation {
 			return filter_var($str, FILTER_VALIDATE_EMAIL);
 		}
 		//this doesn't adhere to RFC822, but is close enough.
-		if (preg_match("/^([\w+-]+)(\.[\w+-]+)*@([\w-]+\.)+[a-zA-Z]{2,6}$/", $str))
-		{
-			return TRUE;
-		}
-		return FALSE;
+		return preg_match("/^([\w+-]+)(\.[\w+-]+)*@([\w-]+\.)+[a-zA-Z]{2,6}$/", $str);
 	}
 
 	// --------------------------------------------------------------------
