c670945206cd89d348736e628a07dd7b08d8c59e
Darren Hill <dchill42@gmail.com>
2011-08-31 14:15:35 -0400

    Missed whitespace on Driver

--- system/libraries/Driver.php
+++ system/libraries/Driver.php
@@ -32,29 +32,29 @@ class CI_Driver_Library {
 	protected $valid_drivers	= array();
 	protected $lib_name;
 
-    /**
-     * Get magic method
-     *
+	/**
+	 * Get magic method
+	 *
 	 * The first time a child is used it won't exist, so we instantiate it
 	 * subsequents calls will go straight to the proper child.
-     *
-     * @param   string  Child class name
-     * @return  object  Child class
-     */
+	 *
+	 * @param   string  Child class name
+	 * @return  object  Child class
+	 */
 	public function __get($child)
 	{
-        // Try to load the driver
+		// Try to load the driver
 		return load_driver($child);
-    }
+	}
 
-    /**
-     * Load driver
-     *
+	/**
+	 * Load driver
+	 *
 	 * Separate load_driver call to support explicit driver load by library or user
-     *
-     * @param   string  Child class name
-     * @return  object  Child class
-     */
+	 *
+	 * @param   string  Child class name
+	 * @return  object  Child class
+	 */
 	public function load_driver($child)
 	{
 		if ( ! isset($this->lib_name))
