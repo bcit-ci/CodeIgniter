31de1fd804858bdf7fd85d7df7c9e3eba9a55d2a
kuboslav <kuboslav@gmail.com>
2011-08-31 15:39:50 +0300

    Validation email address using faster built-in function filter_var.

--- system/helpers/email_helper.php
+++ system/helpers/email_helper.php
@@ -37,7 +37,14 @@ if ( ! function_exists('valid_email'))
 {
 	function valid_email($address)
 	{
-		return ( ! preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix", $address)) ? FALSE : TRUE;
+		if( function_exist('filter_var'))
+		{
+			return (bool) filter_var($address, FILTER_VALIDATE_EMAIL);
+		}
+		else
+		{
+			return ( ! preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix", $address)) ? FALSE : TRUE;	
+		}		
 	}
 }
 
