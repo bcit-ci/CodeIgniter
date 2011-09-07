7f1dd3cd30d787fd5db246dfc35d557497d04071
Adam Jackett <adam@darkhousemedia.com>
2011-09-07 14:07:01 -0400

    Added sess_cookie_fallback option to Session library.

--- application/config/config.php
+++ application/config/config.php
@@ -250,6 +250,7 @@ $config['sess_expire_on_close']	= FALSE;
 $config['sess_encrypt_cookie']	= FALSE;
 $config['sess_use_database']	= FALSE;
 $config['sess_table_name']		= 'ci_sessions';
+$config['sess_cookie_fallback'] = FALSE;
 $config['sess_match_ip']		= FALSE;
 $config['sess_match_useragent']	= TRUE;
 $config['sess_time_to_update']	= 300;
