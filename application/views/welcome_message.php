c770177659f964c98da9f11d804b1433f076ed05
purwandi <free6300@gmail.com>
2011-09-13 11:48:41 +0700

    Fix box shadow, improve css, adding CodeIgniter Version in error_404, error_db, error_general, error_php and welcome_message

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -60,7 +60,9 @@
 	#container{
 		margin: 10px;
 		border: 1px solid #D0D0D0;
+		box-shadow: 0 0 8px #D0D0D0;
 		-webkit-box-shadow: 0 0 8px #D0D0D0;
+		-moz-box-shadow: 0 0 8px #D0D0D0;
 	}
 	</style>
 </head>
