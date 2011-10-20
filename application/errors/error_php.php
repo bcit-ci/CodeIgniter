f4a4bd8fac188ebc9cda822ffc811c218fd92b45
Derek Jones <derek.jones@ellislab.com>
2011-10-20 12:18:42 -0500

    adding new license file (OSL 3.0) and updating readme to ReST
    added notice of license to all source files.
    OSL to all except the few files we ship inside of the application folder, those are AFL.
    Updated license in user guide.
    
    incrementing next dev version to 3.0 due to licensing change

--- application/errors/error_php.php
+++ application/errors/error_php.php
@@ -1,3 +1,31 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP 5.1.6 or newer
+ *
+ * NOTICE OF LICENSE
+ * 
+ * Licensed under the Academic Free License version 3.0
+ * 
+ * This source file is subject to the Open Software License (OSL 3.0) that is
+ * bundled with this package in the files license_afl.txt / license_afl.rst.
+ * It is also available through the world wide web at this URL:
+ * http://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to obtain it
+ * through the world wide web, please send an email to
+ * licensing@ellislab.com so we can send you a copy immediately.
+ *
+ * @package		CodeIgniter
+ * @author		EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2011, EllisLab, Inc. (http://ellislab.com/)
+ * @license		http://opensource.org/licenses/AFL-3.0 Academic Free License (AFL 3.0)
+ * @link		http://codeigniter.com
+ * @since		Version 1.0
+ * @filesource
+ */
+?>
+
 <div style="border:1px solid #990000;padding-left:20px;margin:0 0 10px 0;">
 
 <h4>A PHP Error was encountered</h4>
