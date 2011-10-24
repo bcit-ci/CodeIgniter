e79dfe23d11ac2aa7a870860de2b44d721b20a46
RH Becker <http://rhbecker.info>
2011-10-24 10:55:55 -0700

    Added param to AR join method, allowing escaping to be turned off.

--- system/database/DB_active_rec.php
+++ system/database/DB_active_rec.php
@@ -322,7 +322,7 @@ class CI_DB_active_record extends CI_DB_driver {
 	 * @param	string	the type of join
 	 * @return	object
 	 */
-	public function join($table, $cond, $type = '')
+	public function join($table, $cond, $type = '', $escape = TRUE)
 	{
 		if ($type != '')
 		{
@@ -343,7 +343,7 @@ class CI_DB_active_record extends CI_DB_driver {
 		$this->_track_aliases($table);
 
 		// Strip apart the condition and protect the identifiers
-		if (preg_match('/([\w\.]+)([\W\s]+)(.+)/', $cond, $match))
+		if ($escape && preg_match('/([\w\.]+)([\W\s]+)(.+)/', $cond, $match))
 		{
 			$match[1] = $this->_protect_identifiers($match[1]);
 			$match[3] = $this->_protect_identifiers($match[3]);
