a6b85bc96c63f85eab122e4cc383417d262abf7e
Lucas Nolte <info@91media.de>
2011-11-11 09:33:40 +0100

    Fixed Caching Fallback

--- system/libraries/Cache/Cache.php
+++ system/libraries/Cache/Cache.php
@@ -177,6 +177,10 @@ class CI_Cache extends CI_Driver_Library {
 			{
 				$this->_backup_driver = $config['backup'];
 			}
+			
+			if(!$this->{$this->_adapter}->is_supported()) {
+				$this->_adapter = $this->_backup_driver;
+			}
 		}
 	}
 
