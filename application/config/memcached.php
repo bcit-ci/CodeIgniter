e0039f31f14a6ca63bef6100b103bc651cb98286
Gustav Bertram <gustav.bertram@gmail.com>
2011-11-14 15:19:21 +0200

    Adding a default memcached config file - GWB

--- application/config/memcached.php
+++ application/config/memcached.php
@@ -3,14 +3,19 @@
 | -------------------------------------------------------------------------
 | Memcached settings
 | -------------------------------------------------------------------------
-| Memcached doesn't seem to like hostnames. Try use an IP first. -GWB
+| To add another server, just add another set in the array.
+|
+| NOTE: Memcached doesn't seem to like hostnames. Try using an IP first. 
+| -GWB
 |
 */
 
-$config['default'] = array(
-	'hostname' => '127.0.0.1',
-	'port'     => '11211',
-	'weight'   => '1'        
+$config = array(
+	'default' => array(
+		'hostname' => '127.0.0.1',
+		'port'     => '11211',
+		'weight'   => '1',
+	),      
 );  
 
 /* End of file memcached.php */
