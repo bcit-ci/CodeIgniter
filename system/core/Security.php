7da08133c4b20ee7d56f525f2317dda29c82d4db
Darren Hill <dchill42@gmail.com>
2011-12-09 15:15:03 -0500

    Hotfix for broken _remove_evil_attributes in XSS Security

--- system/core/Security.php
+++ system/core/Security.php
@@ -614,32 +614,17 @@ class CI_Security {
 			unset($evil_attributes[array_search('xmlns', $evil_attributes)]);
 		}
 		
+		// Parse tags, stripping evil attributes
+		// The subpattern (?:([\'"]).*?\2|[^>]) matches everything but >, unless it's in quotes
+		// After the tag open, use this non-greedily (+?) to capture the tag name and non-evil attributes
+		// Then swallow as many evil attributes are found in a row
+		// Finally, use the special subpattern to capture any remaining attributes up to the end of the tag
+		$regex = '/<(\/?(?:([\'"]).*?\2|[^>])+?)'.	// Start open or close tag and capture safe attributes
+			'(?:\s+(?:'.implode('|', $evil_attributes).')\s*=\s*(?:([\'"]).*?\3|[^\s>]*))*'.	// Eat evil attributes
+			'((?:([\'"]).*?\5|[^>])*)>/';			// Capture remaining attributes until end of tag
+		// Wash, rinse, repeat...
 		do {
-			$count = 0;
-			$attribs = array();
-			
-			// find occurrences of illegal attribute strings without quotes
-			preg_match_all("/(".implode('|', $evil_attributes).")\s*=\s*([^\s]*)/is",  $str, $matches, PREG_SET_ORDER);
-			
-			foreach ($matches as $attr)
-			{
-				$attribs[] = preg_quote($attr[0], '/');
-			}
-			
-			// find occurrences of illegal attribute strings with quotes (042 and 047 are octal quotes)
-			preg_match_all("/(".implode('|', $evil_attributes).")\s*=\s*(\042|\047)([^\\2]*?)(\\2)/is",  $str, $matches, PREG_SET_ORDER);
-
-			foreach ($matches as $attr)
-			{
-				$attribs[] = preg_quote($attr[0], '/');
-			}
-
-			// replace illegal attribute strings that are inside an html tag
-			if (count($attribs) > 0)
-			{
-				$str = preg_replace("/<(\/?[^><]+?)([^A-Za-z\-])(".implode('|', $attribs).")([\s><])([><]*)/i", '<$1$2$4$5', $str, -1, $count);
-			}
-			
+			$str = preg_replace($regex, '<$1$4>', $str, -1, $count);
 		} while ($count);
 		
 		return $str;
