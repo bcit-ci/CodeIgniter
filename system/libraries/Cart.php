fd34c8ae71806f0a94b8e7c33d390810524e33ed
Andrew Seymour <commit@andrewseymour.co.uk>
2011-12-13 15:50:52 +0000

    Merged the two if's together as suggested by @philsturgeon - updated the Changelog to include something that may be important

--- system/libraries/Cart.php
+++ system/libraries/Cart.php
@@ -196,13 +196,10 @@ class CI_Cart {
 
 		// Validate the product name. It can only be alpha-numeric, dashes, underscores, colons or periods.
 		// Note: These can be user-specified by setting the $this->product_name_rules variable.
-		if($this->product_name_safe)
+		if ( $this->product_name_safe && ! preg_match("/^[".$this->product_name_rules."]+$/i", $items['name']))
 		{
-			if ( ! preg_match("/^[".$this->product_name_rules."]+$/i", $items['name']))
-			{
-				log_message('error', 'An invalid name was submitted as the product name: '.$items['name'].' The name can only contain alpha-numeric characters, dashes, underscores, colons, and spaces');
-				return FALSE;
-			}
+			log_message('error', 'An invalid name was submitted as the product name: '.$items['name'].' The name can only contain alpha-numeric characters, dashes, underscores, colons, and spaces');
+			return FALSE;
 		}
 
 		// --------------------------------------------------------------------
--- user_guide_src/source/changelog.rst
+++ user_guide_src/source/changelog.rst
@@ -48,6 +48,7 @@ Release Date: Not Released
    -  Modified valid_ip() to use PHP's filter_var() when possible (>= PHP 5.2) in the :doc:`Form Validation library <libraries/form_validation>`.
 	 -  Added custom filename to Email::attach() as $this->email->attach($filename, $disposition, $newname)
    -  The Cart library now auto-increments quantity's instead of just resetting it, this is the default behaviour of large e-commerce sites.
+   -  Cart Product Name strictness can be disabled via the Cart Library by switching "$product_name_safe"
 
 -  Core
 
