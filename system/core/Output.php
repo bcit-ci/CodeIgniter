231e888c67c10126bc939f202bee6a54cf438d9e
Andrey Andreev <narf@bofh.bg>
2012-01-09 11:01:15 +0200

    Remove once again ...

--- system/core/Output.php
+++ system/core/Output.php
@@ -396,7 +396,6 @@ class CI_Output {
 
 			// If the output data contains closing </body> and </html> tags
 			// we will remove them and add them back after we insert the profile data
-			$count = 0;
 			$output = preg_replace('|</body>.*?</html>|is', '', $output, -1, $count).$CI->profiler->run();
 			if ($count > 0)
 			{
