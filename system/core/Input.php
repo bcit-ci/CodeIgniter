1b246c15806b81d4834e6b12d59bfb00823ba6ae
Michiel Vugteveen <michiel@it-can.nl>
2012-01-09 09:12:39 +0100

    rewrite fix issue #33

--- system/core/Input.php
+++ system/core/Input.php
@@ -503,7 +503,7 @@ class CI_Input {
 
 			foreach ($_COOKIE as $key => $val)
 			{
-				$_COOKIE[$this->_clean_input_keys($key)] = $this->_clean_input_data($val);
+				$_COOKIE[$this->_clean_input_keys($key, true)] = $this->_clean_input_data($val, true);
 			}
 		}
 
@@ -530,14 +530,14 @@ class CI_Input {
 	* @param	string
 	* @return	string
 	*/
-	protected function _clean_input_data($str)
+	protected function _clean_input_data($str, $cookie = false)
 	{
 		if (is_array($str))
 		{
 			$new_array = array();
 			foreach ($str as $key => $val)
 			{
-				$new_array[$this->_clean_input_keys($key)] = $this->_clean_input_data($val);
+				$new_array[$this->_clean_input_keys($key, $cookie)] = $this->_clean_input_data($val, $cookie);
 			}
 			return $new_array;
 		}
@@ -588,11 +588,19 @@ class CI_Input {
 	* @param	string
 	* @return	string
 	*/
-	protected function _clean_input_keys($str)
+	protected function _clean_input_keys($str, $cookie = false)
 	{
 		if ( ! preg_match('/^[a-z0-9:_\/-]+$/i', $str))
 		{
-			exit('Disallowed Key Characters.');
+			//if $cookie true will unset it
+			if ($cookie)
+			{
+				unset($_COOKIE[$str]);
+			}
+			else
+			{
+				exit('Disallowed Key Characters.');
+			}
 		}
 
 		// Clean UTF-8 if supported
