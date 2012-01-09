7ed4a3839e7e2f73f8f55818a6247a63c3468599
Andrey Andreev <narf@bofh.bg>
2012-01-09 10:16:41 +0200

    Really fix this ...

--- system/core/Output.php
+++ system/core/Output.php
@@ -396,8 +396,9 @@ class CI_Output {
 
 			// If the output data contains closing </body> and </html> tags
 			// we will remove them and add them back after we insert the profile data
-			$output = preg_replace('|</body>.*?</html>|is', '', $output, $count).$CI->profiler->run();
-			if (isset($count) && $count > 0)
+			$count = 0;
+			$output = preg_replace('|</body>.*?</html>|is', '', $output, -1, $count).$CI->profiler->run();
+			if ($count > 0)
 			{
 				$output .= '</body></html>';
 			}
