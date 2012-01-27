8ebe80d5ddf22ce9073e32dc9de20f22836b1d64
Andrey Andreev <narf@bofh.bg>
2012-01-27 20:50:33 +0200

    Switch _process_fields() from private to protected

--- system/database/drivers/mysqli/mysqli_forge.php
+++ system/database/drivers/mysqli/mysqli_forge.php
@@ -66,7 +66,7 @@ class CI_DB_mysqli_forge extends CI_DB_forge {
 	 * @param	mixed	the fields
 	 * @return	string
 	 */
-	private function _process_fields($fields)
+	public function _process_fields($fields)
 	{
 		$current_field_count = 0;
 		$sql = '';
