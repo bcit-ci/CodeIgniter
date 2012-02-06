7efef4bdedead9727429f11c4c26ba962569d880
Burak Erdem <burakerdem@gmail.com>
2012-02-06 17:28:33 +0200

    Filesystem cache metadata function return correct TTL now.

--- system/libraries/Cache/drivers/Cache_file.php
+++ system/libraries/Cache/drivers/Cache_file.php
@@ -167,13 +167,13 @@ class CI_Cache_file extends CI_Driver {
 		{
 			$mtime = filemtime($this->_cache_path.$id);
 
-			if ( ! isset($data['data']['ttl']))
+			if ( ! isset($data['ttl']))
 			{
 				return FALSE;
 			}
 
 			return array(
-				'expire' => $mtime + $data['data']['ttl'],
+				'expire' => $mtime + $data['ttl'],
 				'mtime'	 => $mtime
 			);
 		}
