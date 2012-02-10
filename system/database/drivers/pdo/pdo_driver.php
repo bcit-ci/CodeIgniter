5cf4e54e8fa4d48b9271eeb70d6c05767ef8f9b0
Timothy Warren <tim@timshomepage.net>
2012-02-10 09:11:59 -0500

    Changed double quotes to single

--- system/database/drivers/pdo/pdo_driver.php
+++ system/database/drivers/pdo/pdo_driver.php
@@ -117,7 +117,7 @@ class CI_DB_pdo_driver extends CI_DB {
 			// $db['hostname'] = 'pdodriver:host(/Server(/DSN))=hostname(/DSN);';
 			// We need to get the prefix (pdodriver used by PDO).
 			$this->dsn = $this->hostname;
-			$split_dsn = explode(":", $this->hostname);
+			$split_dsn = explode(':', $this->hostname);
 			$this->pdodriver = $split_dsn[0];
 			
 			// End this part of the dsn with a semicolon
