25dc75595dd3550a6c20f3d34d8f5259465b545f
Timothy Warren <tim@timshomepage.net>
2012-02-13 14:12:35 -0500

    Re-added if ! empty  clause

--- system/database/drivers/pdo/pdo_driver.php
+++ system/database/drivers/pdo/pdo_driver.php
@@ -121,7 +121,10 @@ class CI_DB_pdo_driver extends CI_DB {
 
 			// End dsn with a semicolon for extra backward compability
 			// if database property was not empty.
-			$this->dsn .= rtrim($this->hostname, ';').';';
+			if( ! empty($this->database))
+			{
+				$this->dsn .= rtrim($this->hostname, ';').';';
+			}
 		}
 		else
 		{
