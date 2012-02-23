f9b87ae533454d9b4031b00e31ca3583bd1151d5
Mike Funk <mfunk@xulonpress.com>
2012-02-23 14:56:18 -0500

    added all_flashdata function to Session.php

--- system/libraries/Session.php
+++ system/libraries/Session.php
@@ -468,6 +468,29 @@ class CI_Session {
 	{
 		return $this->userdata;
 	}
+	
+	// --------------------------------------------------------------------------
+	
+	/**
+	 * Fetch all flashdata
+	 * 
+	 * @return array
+	 */
+	public function all_flashdata()
+	{
+		$out = array();
+		
+		// loop through all userdata
+		foreach ($this->all_userdata() as $key => $val)
+		{	
+			// if it contains flashdata, add it
+			if (strpos($key, 'flash:old:') !== FALSE)
+			{
+				$out[$key] = $val;
+			}
+		}
+		return $out;
+	}
 
 	// --------------------------------------------------------------------
 
