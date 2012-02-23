64d72b1b85efd3810a036b54d40d36ade6cd9f86
Mike Funk <mfunk@xulonpress.com>
2012-02-23 15:07:21 -0500

    removed active_rec change.

--- system/database/DB_active_rec.php
+++ system/database/DB_active_rec.php
@@ -988,7 +988,7 @@ class CI_DB_active_record extends CI_DB_driver {
 	 */
 	public function limit($value, $offset = NULL)
 	{
-		$this->ar_limit = $value;
+		$this->ar_limit = (int) $value;
 
 		if ( ! is_null($offset))
 		{
