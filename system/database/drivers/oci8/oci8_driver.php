08c7c62b57b0d6d8f126e8629b8e8da71bd9636f
Andrey Andreev <narf@bofh.bg>
2012-02-28 13:23:38 +0200

    Fix escape_like_str()

--- system/database/drivers/oci8/oci8_driver.php
+++ system/database/drivers/oci8/oci8_driver.php
@@ -477,8 +477,8 @@ class CI_DB_oci8_driver extends CI_DB {
 		// escape LIKE condition wildcards
 		if ($like === TRUE)
 		{
-			return str_replace(array('%', '_', $this->_like_escape_chr),
-						array($this->_like_escape_chr.'%', $this->_like_escape_chr.'_', $this->_like_escape_chr.$this->_like_escape_chr),
+			return str_replace(array($this->_like_escape_chr.$this->_like_escape_chr, '%', '_'),
+						array($this->_like_escape_chr.$this->_like_escape_chr, $this->_like_escape_chr.'%', $this->_like_escape_chr.'_'),
 						$str);
 		}
 
