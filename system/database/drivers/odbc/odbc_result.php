132c80b65c630e18312f778e4a2d8fafa552db93
Andrey Andreev <narf@bofh.bg>
2012-02-29 16:43:57 +0200

    Fix ODBC num_rows() for subdrivers that return -1

--- system/database/drivers/odbc/odbc_result.php
+++ system/database/drivers/odbc/odbc_result.php
@@ -43,7 +43,18 @@ class CI_DB_odbc_result extends CI_DB_result {
 	 */
 	public function num_rows()
 	{
-		return @odbc_num_rows($this->result_id);
+		if ($this->num_rows > 0)
+		{
+			return $this->num_rows;
+		}
+
+		// Work-around for ODBC subdrivers that don't support num_rows()
+		if (($this->num_rows = @odbc_num_rows($this->result_id)) === -1)
+		{
+			return $this->num_rows = count($this->result_array());
+		}
+
+		return $this->num_rows;
 	}
 
 	/**
