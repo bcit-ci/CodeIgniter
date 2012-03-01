dcb4091c27aef012d47779dfc32d0f3121093044
Andrey Andreev <narf@bofh.bg>
2012-03-01 19:50:26 +0200

    Remove SQLite3 db_set_charset() - no longer needed

--- system/database/drivers/sqlite3/sqlite3_driver.php
+++ system/database/drivers/sqlite3/sqlite3_driver.php
@@ -120,21 +120,6 @@ class CI_DB_sqlite3_driver extends CI_DB {
 	// --------------------------------------------------------------------
 
 	/**
-	 * Set client character set
-	 *
-	 * @param	string
-	 * @param	string
-	 * @return	bool
-	 */
-	public function db_set_charset($charset, $collation)
-	{
-		// Not (natively) supported
-		return TRUE;
-	}
-
-	// --------------------------------------------------------------------
-
-	/**
 	 * Version number query string
 	 *
 	 * @return	string
