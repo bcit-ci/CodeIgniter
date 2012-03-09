bb40516f883167a64ddfb2734296017a43771d96
tiyowan <tiyowan@gmail.com>
2012-03-09 17:59:26 +0400

    Fix indentation
    Signed-off-by: tiyowan <tiyowan@gmail.com>

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -6,17 +6,13 @@ error_reporting(E_ALL | E_STRICT);
 
 $dir = realpath(dirname(__FILE__));
 
-
 // Path constants
 define('PROJECT_BASE',	realpath($dir.'/../').'/');
 define('BASEPATH',		PROJECT_BASE.'system/');
 define('APPPATH',		PROJECT_BASE.'application/');
 
-
 // Prep our test environment
 require_once $dir.'/lib/common.php';
 require_once $dir.'/lib/ci_testcase.php';
-
-
 
 unset($dir);
