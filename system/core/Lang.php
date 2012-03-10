b34db884a479d029ea4e166e223640f2821e88ba
Brad Estey <brad@setsolid.net>
2012-03-10 11:32:57 -0500

    Made it possible to pass an array into the load method (Because it said it was possible.. but it was not.) Removed unneeded str_replace if  is TRUE. Added a much needed clear method to remove individual language files or all language files from the  array. So now you can load two versions of a single language file on the same page.

--- system/core/Lang.php
+++ system/core/Lang.php
@@ -62,72 +62,90 @@ class CI_Lang {
 	 * @param 	string	alternative path to look for language file
 	 * @return	mixed
 	 */
-	function load($langfile = '', $idiom = '', $return = FALSE, $add_suffix = TRUE, $alt_path = '')
+	function load($langfiles = array(), $idiom = '', $return = FALSE, $add_suffix = TRUE, $alt_path = '')
 	{
-		$langfile = str_replace('.php', '', $langfile);
-
-		if ($add_suffix == TRUE)
+		if ( ! is_array($langfiles))
 		{
-			$langfile = str_replace('_lang.', '', $langfile).'_lang';
+			$langfiles = array($langfiles);
 		}
-
-		$langfile .= '.php';
-
-		if (in_array($langfile, $this->is_loaded, TRUE))
-		{
-			return;
+		
+		if ($return == TRUE)
+		{		
+			$return_array = array();
 		}
-
-		$config =& get_config();
-
-		if ($idiom == '')
+		
+		foreach($langfiles as $langfile)
 		{
-			$deft_lang = ( ! isset($config['language'])) ? 'english' : $config['language'];
-			$idiom = ($deft_lang == '') ? 'english' : $deft_lang;
-		}
-
-		// Determine where the language file is and load it
-		if ($alt_path != '' && file_exists($alt_path.'language/'.$idiom.'/'.$langfile))
-		{
-			include($alt_path.'language/'.$idiom.'/'.$langfile);
-		}
-		else
-		{
-			$found = FALSE;
-
-			foreach (get_instance()->load->get_package_paths(TRUE) as $package_path)
+			$langfile = str_replace('.php', '', $langfile);
+	
+			if ($add_suffix == TRUE)
 			{
-				if (file_exists($package_path.'language/'.$idiom.'/'.$langfile))
+				$langfile .= '_lang';
+			}
+	
+			$langfile .= '.php';
+	
+			if (in_array($langfile, $this->is_loaded, TRUE))
+			{
+				continue;
+			}
+	
+			$config =& get_config();
+	
+			if ($idiom == '')
+			{
+				$deft_lang = ( ! isset($config['language'])) ? 'english' : $config['language'];
+				$idiom = ($deft_lang == '') ? 'english' : $deft_lang;
+			}
+	
+			// Determine where the language file is and load it
+			if ($alt_path != '' && file_exists($alt_path.'language/'.$idiom.'/'.$langfile))
+			{
+				include($alt_path.'language/'.$idiom.'/'.$langfile);
+			}
+			else
+			{
+				$found = FALSE;
+	
+				foreach (get_instance()->load->get_package_paths(TRUE) as $package_path)
 				{
-					include($package_path.'language/'.$idiom.'/'.$langfile);
-					$found = TRUE;
-					break;
+					if (file_exists($package_path.'language/'.$idiom.'/'.$langfile))
+					{
+						include($package_path.'language/'.$idiom.'/'.$langfile);
+						$found = TRUE;
+						break;
+					}
+				}
+	
+				if ($found !== TRUE)
+				{
+					show_error('Unable to load the requested language file: language/'.$idiom.'/'.$langfile);
 				}
 			}
-
-			if ($found !== TRUE)
+	
+	
+			if ( ! isset($lang))
 			{
-				show_error('Unable to load the requested language file: language/'.$idiom.'/'.$langfile);
+				log_message('error', 'Language file contains no data: language/'.$idiom.'/'.$langfile);
+				return;
 			}
-		}
-
-
-		if ( ! isset($lang))
-		{
-			log_message('error', 'Language file contains no data: language/'.$idiom.'/'.$langfile);
-			return;
+	
+			if ($return == TRUE)
+			{			
+				$return_array = array_merge($return_array, $lang);
+			}
+	
+			$this->is_loaded[] = $langfile;
+			$this->language = array_merge($this->language, $lang);
+			unset($lang);
+			log_message('debug', 'Language file loaded: language/'.$idiom.'/'.$langfile);
 		}
 
 		if ($return == TRUE)
 		{
-			return $lang;
+			return $return_array;
 		}
-
-		$this->is_loaded[] = $langfile;
-		$this->language = array_merge($this->language, $lang);
-		unset($lang);
-
-		log_message('debug', 'Language file loaded: language/'.$idiom.'/'.$langfile);
+		
 		return TRUE;
 	}
 
@@ -151,6 +169,54 @@ class CI_Lang {
 		}
 
 		return $value;
+	}
+	
+	// --------------------------------------------------------------------
+
+	/**
+	 * Unload a single language file or all language files
+	 *
+	 * @access	public
+	 * @param	mixed	the name of the specific language file to be removed from the 'is_loaded' array. Can be an array
+	 * @param 	bool	add suffix to $langfile
+	 * @return	mixed
+	 */	
+	function clear($langfiles = array(), $add_suffix = TRUE)
+	{
+		if (empty($langfiles))
+		{
+			$this->is_loaded = array();
+			return TRUE;
+		}
+		
+		if ( ! is_array($langfiles))
+		{
+			$langfiles = array($langfiles);
+		}
+
+		foreach($langfiles as $langfile)
+		{		
+			$langfile = str_replace('.php', '', $langfile);	
+		
+			if ($add_suffix == TRUE)
+			{
+				$langfile .= '_lang';
+			}
+
+			$langfile .= '.php';
+			
+			// Determine if the language file is loaded
+			$key = array_search($langfile, $this->is_loaded);
+		
+			if ($key === FALSE)
+			{
+				log_message('error', 'Could not find the language file "'.$langfile.'"');
+				return;
+			}
+		
+			unset($this->is_loaded[$key]);	
+		}
+		return TRUE;		
 	}
 
 }
