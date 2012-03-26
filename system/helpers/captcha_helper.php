668f78365a468dfc2508a3743ca16c1d421b8f53
Andrey Andreev <narf@bofh.bg>
2012-03-26 21:53:37 +0300

    Merge upstream branch

--- system/helpers/captcha_helper.php
+++ system/helpers/captcha_helper.php
@@ -101,7 +101,6 @@ if ( ! function_exists('create_captcha'))
 			{
 				$word .= $pool[mt_rand(0, $mt_rand_max)];
 			}
-
 		}
 
 		// -----------------------------------
@@ -121,10 +120,10 @@ if ( ! function_exists('create_captcha'))
 		// -----------------------------------
 		//  Assign colors
 		// -----------------------------------
-		$bg_color		= imagecolorallocate ($im, 255, 255, 255);
-		$border_color	= imagecolorallocate ($im, 153, 102, 102);
-		$text_color		= imagecolorallocate ($im, 204, 153, 153);
-		$grid_color		= imagecolorallocate($im, 255, 182, 182);
+		$bg_color	= imagecolorallocate($im, 255, 255, 255);
+		$border_color	= imagecolorallocate($im, 153, 102, 102);
+		$text_color	= imagecolorallocate($im, 204, 153, 153);
+		$grid_color	= imagecolorallocate($im, 255, 182, 182);
 		$shadow_color	= imagecolorallocate($im, 255, 240, 240);
 
 		//  Create the rectangle
