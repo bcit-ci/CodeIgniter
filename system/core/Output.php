9a741eae9f79ec5190c566f4cc4812a5791992f4
Songpol Sripaoeiam <sripaoeiam@gmail.com>
2012-04-01 17:13:44 +0700

    bracket on a new line
    Also add space after foreach
    
    bracket on a new line... Also add a space after if
    
    Follow up from https://github.com/EllisLab/CodeIgniter/pull/1234/files#r630522

--- system/core/Output.php
+++ system/core/Output.php
@@ -231,12 +231,13 @@ class CI_Output {
 	 */
 	public function get_current_content_type()
 	{
-			foreach($this->headers as $header){
-				if(preg_match('/^Content-Type/', $header[0])){
-					return str_replace('Content-Type: ', '', $header[0]);
-				}
+		foreach ($this->headers as $header){
+			if (preg_match('/^Content-Type/', $header[0]))
+			{
+				return str_replace('Content-Type: ', '', $header[0]);
 			}
-			return 'text/html';
+		}
+		return 'text/html';
 	}
 
 	// --------------------------------------------------------------------
