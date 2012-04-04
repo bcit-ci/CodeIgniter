44015c8d91ecea78ee4a10de579c36a859c2fcb6
Taufan Aditya <toopay@taufanaditya.com>
2012-04-04 19:38:16 +0700

    Set up PDO constant and configuration

--- tests/mocks/database/config/sqlite.php
+++ tests/mocks/database/config/sqlite.php
@@ -1,5 +1,4 @@
 <?php
-$dbdriver = is_php('5.4') ? 'sqlite3' : 'sqlite';
 
 return array(
 
@@ -10,7 +9,7 @@ return array(
 		'username' => 'sqlite',
 		'password' => 'sqlite',
 		'database' => realpath(__DIR__.'/..').'/ci_test.sqlite',
-		'dbdriver' => $dbdriver,
+		'dbdriver' => is_php('5.4') ? 'sqlite3' : 'sqlite',
 	),
 
 	// Database configuration with failover
@@ -20,7 +19,7 @@ return array(
 		'username' => 'sqlite',
 		'password' => 'sqlite',
 		'database' => '../not_exists.sqlite',
-		'dbdriver' => $dbdriver,
+		'dbdriver' => is_php('5.4') ? 'sqlite3' : 'sqlite',
 		'failover' => array(
 			array(
 				'dsn' => '',
@@ -28,7 +27,7 @@ return array(
 				'username' => 'sqlite',
 				'password' => 'sqlite',
 				'database' => realpath(__DIR__.'/..').'/ci_testf.sqlite',
-				'dbdriver' => $dbdriver,
+				'dbdriver' => is_php('5.4') ? 'sqlite3' : 'sqlite',
 			),
 		),
 	),
