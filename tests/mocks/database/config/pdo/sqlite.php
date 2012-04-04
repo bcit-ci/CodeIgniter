fe68505f51ab9646c1197419d68392b5806e75a5
Taufan Aditya <toopay@taufanaditya.com>
2012-04-04 23:46:23 +0700

    Fix sqlite issue on PHP 5.4 both for native and PDO driver

--- tests/mocks/database/config/pdo/sqlite.php
+++ tests/mocks/database/config/pdo/sqlite.php
@@ -4,20 +4,32 @@ return array(
 
 	// Typical Database configuration
 	'pdo/sqlite' => array(
-		'dsn' => 'sqlite:/'.realpath(__DIR__.'/..').'/ci_test.sqlite',
+		'dsn' => 'sqlite:/'.realpath(__DIR__.'/../..').'/ci_test.sqlite',
+		'hostname' => 'localhost',
+		'username' => 'sqlite',
+		'password' => 'sqlite',
+		'database' => 'sqlite',
 		'dbdriver' => 'pdo',
 		'pdodriver' => 'sqlite',
 	),
 
 	// Database configuration with failover
 	'pdo/sqlite_failover' => array(
-		'dsn' => 'sqlite:/'.realpath(__DIR__.'/..').'/not_exists.sqlite',
+		'dsn' => 'sqlite:not_exists.sqlite',
+		'hostname' => 'localhost',
+		'username' => 'sqlite',
+		'password' => 'sqlite',
+		'database' => 'sqlite',
 		'dbdriver' => 'pdo',
 		'pdodriver' => 'sqlite',
 		'failover' => array(
 			array(
-				'dsn' => 'sqlite:/'.realpath(__DIR__.'/..').'/ci_test.sqlite',
-				'dbdriver' => 'pdo',
+				'dsn' => 'sqlite:/'.realpath(__DIR__.'/../..').'/ci_test.sqlite',
+				'hostname' => 'localhost',
+				'username' => 'sqlite',
+				'password' => 'sqlite',
+				'database' => 'sqlite',
+				'dbdriver' => 'pdo', 
 				'pdodriver' => 'sqlite',
 			),
 		),
