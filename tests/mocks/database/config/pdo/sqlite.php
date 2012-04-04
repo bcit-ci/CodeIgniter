f4c6c9b3061b464c959b6409f962228959f35287
Taufan Aditya <toopay@taufanaditya.com>
2012-04-04 23:24:09 +0700

    DB Drivers test

--- tests/mocks/database/config/pdo/sqlite.php
+++ tests/mocks/database/config/pdo/sqlite.php
@@ -6,16 +6,19 @@ return array(
 	'pdo/sqlite' => array(
 		'dsn' => 'sqlite:/'.realpath(__DIR__.'/..').'/ci_test.sqlite',
 		'dbdriver' => 'pdo',
+		'pdodriver' => 'sqlite',
 	),
 
 	// Database configuration with failover
 	'pdo/sqlite_failover' => array(
 		'dsn' => 'sqlite:/'.realpath(__DIR__.'/..').'/not_exists.sqlite',
 		'dbdriver' => 'pdo',
+		'pdodriver' => 'sqlite',
 		'failover' => array(
 			array(
 				'dsn' => 'sqlite:/'.realpath(__DIR__.'/..').'/ci_test.sqlite',
 				'dbdriver' => 'pdo',
+				'pdodriver' => 'sqlite',
 			),
 		),
 	),
