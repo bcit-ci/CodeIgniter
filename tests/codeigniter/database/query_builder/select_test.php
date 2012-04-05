e67d452843c256202eefe8e9a2fb493ac7db4a44
Taufan Aditya <toopay@taufanaditya.com>
2012-04-05 19:08:00 +0700

    Min, Max, Avg and Sum

--- tests/codeigniter/database/query_builder/select_test.php
+++ tests/codeigniter/database/query_builder/select_test.php
@@ -33,5 +33,65 @@ class Select_test extends CI_TestCase {
 		$this->assertFalse(array_key_exists('id', $jobs_name[0]));
 		$this->assertFalse(array_key_exists('description', $jobs_name[0]));
 	}
+
+	// ------------------------------------------------------------------------
+
+	/**
+	 * @see ./mocks/schema/skeleton.php
+	 */
+	public function test_select_min()
+	{
+		$job_min = $this->query_builder->select_min('id')
+		                               ->get('job')
+		                               ->result_array();
+		
+		// Minimum id was 1
+		$this->assertEquals('1', $job_min[0]['id']);
+	}
+
+	// ------------------------------------------------------------------------
+
+	/**
+	 * @see ./mocks/schema/skeleton.php
+	 */
+	public function test_select_max()
+	{
+		$job_max = $this->query_builder->select_max('id')
+		                               ->get('job')
+		                               ->result_array();
+		
+		// Maximum id was 4
+		$this->assertEquals('4', $job_max[0]['id']);
+	}
+
+	// ------------------------------------------------------------------------
+
+	/**
+	 * @see ./mocks/schema/skeleton.php
+	 */
+	public function test_select_avg()
+	{
+		$job_avg = $this->query_builder->select_avg('id')
+		                               ->get('job')
+		                               ->result_array();
+		
+		// Average should be 2.5
+		$this->assertEquals('2.5', $job_avg[0]['id']);
+	}
+
+	// ------------------------------------------------------------------------
+
+	/**
+	 * @see ./mocks/schema/skeleton.php
+	 */
+	public function test_select_sum()
+	{
+		$job_sum = $this->query_builder->select_sum('id')
+		                               ->get('job')
+		                               ->result_array();
+		
+		// Sum of ids should be 10
+		$this->assertEquals('10', $job_sum[0]['id']);
+	}
 	
 }
