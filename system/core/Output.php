0f2211711deceb74157d6811116acc0376d3157d
Andrey Andreev <narf@bofh.bg>
2012-04-13 14:52:16 +0300

    Switch erroneously declared protected properties in Output class to public

--- system/core/Output.php
+++ system/core/Output.php
@@ -43,37 +43,37 @@ class CI_Output {
 	 *
 	 * @var string
 	 */
-	protected $final_output;
+	public $final_output;
 	/**
 	 * Cache expiration time
 	 *
 	 * @var int
 	 */
-	protected $cache_expiration	= 0;
+	public $cache_expiration	= 0;
 	/**
 	 * List of server headers
 	 *
 	 * @var array
 	 */
-	protected $headers			= array();
+	public $headers			= array();
 	/**
 	 * List of mime types
 	 *
 	 * @var array
 	 */
-	protected $mime_types		= array();
+	public $mime_types		= array();
 	/**
 	 * Determines wether profiler is enabled
 	 *
 	 * @var book
 	 */
-	protected $enable_profiler	= FALSE;
+	public $enable_profiler		= FALSE;
 	/**
 	 * Determines if output compression is enabled
 	 *
 	 * @var bool
 	 */
-	protected $_zlib_oc			= FALSE;
+	protected $_zlib_oc		= FALSE;
 	/**
 	 * List of profiler sections
 	 *
@@ -85,7 +85,7 @@ class CI_Output {
 	 *
 	 * @var bool
 	 */
-	protected $parse_exec_vars	= TRUE;
+	public $parse_exec_vars		= TRUE;
 
 	public function __construct()
 	{
