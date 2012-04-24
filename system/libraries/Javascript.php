818e2e8b7284630c32fd47ccb03413c4375f13c5
Cosmin Atanasiu <innorogue@gmail.com>
2012-04-23 20:56:37 -0700

    Update system/libraries/Javascript.php

--- system/libraries/Javascript.php
+++ system/libraries/Javascript.php
@@ -723,7 +723,7 @@ class CI_Javascript {
 		{
 			if (is_object($result))
 			{
-				$json_result = $result->result_array();
+				$json_result = (array)$result;
 			}
 			elseif (is_array($result))
 			{
