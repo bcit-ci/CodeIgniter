8522ccc832142ff4173d7dd9fe0bbc3a83377490
Taufan Aditya <toopay@taufanaditya.com>
2012-05-07 18:09:18 +0700

    Replace white-space with tab

--- tests/application/Welcome_test.php
+++ tests/application/Welcome_test.php
@@ -6,16 +6,16 @@ class Welcome_test extends CI_Selenium_TestCase {
 	{
 		$this->setBrowser('firefox');
 		
-        // Change below url to your servername, eg : $this->url('http://codeigniter.dev/');
-        $this->setBrowserUrl('http://127.0.0.1/codeigniter/index.php');
-        $this->setBrowserUrl('http://codeigniter.dev/');
+		// Change below url to your servername, eg : $this->url('http://codeigniter.dev/');
+		$this->setBrowserUrl('http://127.0.0.1/codeigniter/index.php');
+		$this->setBrowserUrl('http://codeigniter.dev/');
 	}
 	
 	// ------------------------------------------------------------------------
 	
 	public function test_title()
 	{
-        // Change below url to your servername, eg : $this->url('http://codeigniter.dev/');
+		// Change below url to your servername, eg : $this->url('http://codeigniter.dev/');
 		$this->url('http://127.0.0.1/codeigniter/index.php');
 
 		$this->assertEquals('Welcome to CodeIgniter', $this->title());
@@ -25,7 +25,7 @@ class Welcome_test extends CI_Selenium_TestCase {
 	
 	public function test_body()
 	{
-        // Change below url to your servername, eg : $this->url('http://codeigniter.dev/');
+		// Change below url to your servername, eg : $this->url('http://codeigniter.dev/');
 		$this->url('http://127.0.0.1/codeigniter/index.php');
 
 		$expected_body = "The page you are looking at is being generated dynamically by CodeIgniter.
@@ -44,11 +44,11 @@ If you are exploring CodeIgniter for the very first time, you should start by re
 	
 	public function test_footer()
 	{
-        // Change below url to your servername, eg : $this->url('http://codeigniter.dev/');
+		// Change below url to your servername, eg : $this->url('http://codeigniter.dev/');
 		$this->url('http://127.0.0.1/codeigniter/index.php');
 
 		$footer_pattern = "/^Page rendered in (.+) seconds. CodeIgniter Version (.+)$/";
-		
+
 		$footer = $this->byClassName('footer');
 
 		$this->assertEquals(1, preg_match($footer_pattern, $footer->text()));
