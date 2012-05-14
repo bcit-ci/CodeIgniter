09069ddcecde9a0c66e41087e816567d420eb750
Timothy Warren <tim@timshomepage.net>
2012-05-14 15:21:33 -0400

    Move errors to views folder

--- system/core/Exceptions.php
+++ system/core/Exceptions.php
@@ -141,7 +141,7 @@ class CI_Exceptions {
 			ob_end_flush();
 		}
 		ob_start();
-		include(APPPATH.'errors/'.$template.'.php');
+		include(APPPATH.'views/errors/'.$template.'.php');
 		$buffer = ob_get_contents();
 		ob_end_clean();
 		return $buffer;
@@ -175,7 +175,7 @@ class CI_Exceptions {
 			ob_end_flush();
 		}
 		ob_start();
-		include(APPPATH.'errors/'.'error_php.php');
+		include(APPPATH.'views/errors/error_php.php');
 		$buffer = ob_get_contents();
 		ob_end_clean();
 		echo $buffer;
