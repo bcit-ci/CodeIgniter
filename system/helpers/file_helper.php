e84e04cae7a48eba4188a81f7100bdbc9eeec43f
Phil Sturgeon <email@philsturgeon.co.uk>
2012-06-04 16:51:14 -0500

    Direct return from mimes config, instead of using global $mimes; Global variables are generally a terrible idea, especially for something as simple as this. The mimes.php now returns an array instead of just injecting a variable name into the global namespace.

--- system/helpers/file_helper.php
+++ system/helpers/file_helper.php
@@ -349,17 +349,17 @@ if ( ! function_exists('get_mime_by_extension'))
 	{
 		$extension = strtolower(substr(strrchr($file, '.'), 1));
 
-		global $mimes;
+		static $mimes;
 
 		if ( ! is_array($mimes))
 		{
 			if (defined('ENVIRONMENT') && is_file(APPPATH.'config/'.ENVIRONMENT.'/mimes.php'))
 			{
-				include(APPPATH.'config/'.ENVIRONMENT.'/mimes.php');
+				$mimes = include(APPPATH.'config/'.ENVIRONMENT.'/mimes.php');
 			}
 			elseif (is_file(APPPATH.'config/mimes.php'))
 			{
-				include(APPPATH.'config/mimes.php');
+				$mimes = include(APPPATH.'config/mimes.php');
 			}
 
 			if ( ! is_array($mimes))
