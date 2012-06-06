c28b651b91367e86f1bd6ab7f0cd6c45e58811ab
Joffrey Jaffeux <j.jaffeux@gmail.com>
2012-06-06 14:26:04 +0200

    Add support for ipv6

--- system/core/Input.php
+++ system/core/Input.php
@@ -383,7 +383,27 @@ class CI_Input {
 	 */
 	public function valid_ip($ip)
 	{
-		return (bool) filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4);
+		if($this->ip_version($ip) === '4')
+		{
+			return (bool) filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4);
+		}
+		else
+		{
+			return (bool) filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6);
+		}
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * Return ip version
+	 *
+	 * @param	string
+	 * @return	string
+	 */
+	public function ip_version($ip)
+	{
+		return strpos($ip, ":") === false ? '4' : '6';
 	}
 
 	// --------------------------------------------------------------------
