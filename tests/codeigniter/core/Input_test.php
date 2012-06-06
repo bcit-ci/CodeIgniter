1ab6f6520ebfc016c49cfbe3a4d9d009be5da268
Joffrey Jaffeux <j.jaffeux@gmail.com>
2012-06-06 20:14:13 +0200

    removed ip_version()

--- tests/codeigniter/core/Input_test.php
+++ tests/codeigniter/core/Input_test.php
@@ -158,18 +158,4 @@ class Input_test extends CI_TestCase {
 		}
 	}
 
-	// --------------------------------------------------------------------
-	
-	public function test_ip_version()
-	{
-		$ip_v4 = '192.18.0.1';
-		$this->assertEquals(4, $this->input->ip_version($ip_v4));
-
-		$ip_v6 = array('2001:0db8:0000:85a3:0000:0000:ac1f:8001', '2001:db8:0:85a3:0:0:ac1f:8001', '2001:db8:0:85a3::ac1f:8001');
-		foreach($ip_v6 as $ip)
-		{
-			$this->assertEquals(6, $this->input->ip_version($ip));
-		}
-	}
-
 }
