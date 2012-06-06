83c62ccf881847c5f4eef83822de0f7b09f98491
Joffrey Jaffeux <j.jaffeux@gmail.com>
2012-06-06 14:26:31 +0200

    add tests for ipv6 and new method ip_version()

--- tests/codeigniter/core/Input_test.php
+++ tests/codeigniter/core/Input_test.php
@@ -143,4 +143,31 @@ class Input_test extends CI_TestCase {
 		$this->assertEquals("Hello, i try to <script>alert('Hack');</script> your site", $harm);
 		$this->assertEquals("Hello, i try to [removed]alert&#40;'Hack'&#41;;[removed] your site", $harmless);
 	}
+
+	// --------------------------------------------------------------------
+	
+	public function test_valid_ip()
+	{
+		$ip_v4 = '175.123.74.43';
+		$this->assertTrue($this->input->valid_ip($ip_v4));
+
+		$ip_v6 = array('2001:0db8:0000:85a3:0000:0000:ac1f:8001', '2001:db8:0:85a3:0:0:ac1f:8001', '2001:db8:0:85a3::ac1f:8001');
+		foreach($ip_v6 as $ip) {
+			$this->assertTrue($this->input->valid_ip($ip));
+		}
+	}
+
+	// --------------------------------------------------------------------
+	
+	public function test_ip_version()
+	{
+		$ip_v4 = '175.123.74.43';
+		$this->assertEquals('4', $this->input->ip_version($ip_v4));
+
+		$ip_v6 = array('2001:0db8:0000:85a3:0000:0000:ac1f:8001', '2001:db8:0:85a3:0:0:ac1f:8001', '2001:db8:0:85a3::ac1f:8001');
+		foreach($ip_v6 as $ip) {
+			$this->assertEquals('6', $this->input->ip_version($ip));
+		}
+	}
+
 }
