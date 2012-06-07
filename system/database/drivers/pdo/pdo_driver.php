aedcbf6f80bbee09108f246e56ac203a34490ce8
Michał Śniatała <m.sniatala@gmail.com>
2012-06-08 00:50:05 +0300

    Transaction "test mode" fix

--- system/database/drivers/pdo/pdo_driver.php
+++ system/database/drivers/pdo/pdo_driver.php
@@ -292,7 +292,7 @@ class CI_DB_pdo_driver extends CI_DB {
 		// Reset the transaction failure flag.
 		// If the $test_mode flag is set to TRUE transactions will be rolled back
 		// even if the queries produce a successful result.
-		$this->_trans_failure = ($test_mode === TRUE);
+		$this->_trans_status = ($test_mode === TRUE) ? FALSE : $this->_trans_status;
 
 		return $this->conn_id->beginTransaction();
 	}
