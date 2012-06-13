ea6cbe026b73be13dd853d710b9609e3faf50f44
Andrey Andreev <narf@bofh.bg>
2012-06-13 23:37:04 +0300

    Revert a preg_replace()

--- system/database/DB_driver.php
+++ system/database/DB_driver.php
@@ -1263,7 +1263,7 @@ class CI_DB_driver {
 		}
 
 		// Convert tabs or multiple spaces into single spaces
-		$item = preg_replace('/\s+/', ' ', $item);
+		$item = preg_replace('/[\t ]+/', ' ', $item);
 
 		// If the item has an alias declaration we remove it and set it aside.
 		// Basically we remove everything to the right of the first space
