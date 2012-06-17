9f18f48fb838c35cf7670b7653e851530e9500a2
Iban Eguia <admin@razican.com>
2012-06-17 19:00:02 +0200

    Added a function to the session library to be able to change the expiration time for the current session.

--- system/libraries/Session.php
+++ system/libraries/Session.php
@@ -579,6 +579,50 @@ class CI_Session {
 	// --------------------------------------------------------------------
 
 	/**
+	 * Change the expiration time of the current session
+	 *
+	 * @return	void
+	 */
+	public function set_expiration($seconds = 0)
+	{
+		if ($seconds !== 0)
+		{
+			$expire						= time() + $seconds;
+			$this->sess_expire_on_close	= FALSE;
+			$this->sess_expiration		= $seconds;
+		}
+		else
+		{
+			$this->sess_expire_on_close	= TRUE;
+		}
+
+		// Serialize the userdata for the cookie
+		$cookie_data = $this->_serialize($this->userdata);
+
+		if ($this->sess_encrypt_cookie == TRUE)
+		{
+			$cookie_data = $this->CI->encrypt->encode($cookie_data);
+		}
+		else
+		{
+			// if encryption is not used, we provide an md5 hash to prevent userside tampering
+			$cookie_data = $cookie_data.md5($cookie_data.$this->encryption_key);
+		}
+
+		// Set the cookie
+		setcookie(
+					$this->sess_cookie_name,
+					$cookie_data,
+					$expire,
+					$this->cookie_path,
+					$this->cookie_domain,
+					$this->cookie_secure
+				);
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
 	 * Fetch a specific item from the session array
 	 *
 	 * @param	string
