a6ea5721bc34fe8666d403637e3bd84772e73566
Andrey Andreev <narf@bofh.bg>
2012-07-02 16:33:11 +0300

    Fix issue #1551

--- system/helpers/date_helper.php
+++ system/helpers/date_helper.php
@@ -124,16 +124,16 @@ if ( ! function_exists('standard_date'))
 	function standard_date($fmt = 'DATE_RFC822', $time = '')
 	{
 		$formats = array(
-				'DATE_ATOM'		=>	'%Y-%m-%dT%H:%i:%s%O',
+				'DATE_ATOM'		=>	'%Y-%m-%dT%H:%i:%s%P',
 				'DATE_COOKIE'	=>	'%l, %d-%M-%y %H:%i:%s UTC',
-				'DATE_ISO8601'	=>	'%Y-%m-%dT%H:%i:%s%O',
+				'DATE_ISO8601'	=>	'%Y-%m-%dT%H:%i:%s%P',
 				'DATE_RFC822'	=>	'%D, %d %M %y %H:%i:%s %O',
 				'DATE_RFC850'	=>	'%l, %d-%M-%y %H:%i:%s UTC',
 				'DATE_RFC1036'	=>	'%D, %d %M %y %H:%i:%s %O',
 				'DATE_RFC1123'	=>	'%D, %d %M %Y %H:%i:%s %O',
-				'DATE_RFC2822'	=>	'%D, %d %M %Y %H:%i:%s %O',
+				'DATE_RFC2822'	=>	'%r',
 				'DATE_RSS'		=>	'%D, %d %M %Y %H:%i:%s %O',
-				'DATE_W3C'		=>	'%Y-%m-%dT%H:%i:%s%O'
+				'DATE_W3C'		=>	'%Y-%m-%dT%H:%i:%s%P'
 				);
 
 		return isset($formats[$fmt]) ? mdate($formats[$fmt], $time) : FALSE;
