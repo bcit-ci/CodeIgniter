bd0b4b306fd9995e19c333eb9b55806e9b56f34c
Andrey Andreev <narf@bofh.bg>
2012-07-02 15:48:35 +0300

    Fix issue #1543

--- system/libraries/Cache/drivers/Cache_file.php
+++ system/libraries/Cache/drivers/Cache_file.php
@@ -151,13 +151,12 @@ class CI_Cache_file extends CI_Driver {
 		{
 			return FALSE;
 		}
-		
-		$data = read_file($this->_cache_path.$id);		
+
+		$data = read_file($this->_cache_path.$id);
 		$data = unserialize($data);
-		
+
 		if (is_array($data))
 		{
-			$data = $data['data'];
 			$mtime = filemtime($this->_cache_path.$id);
 
 			if ( ! isset($data['ttl']))
@@ -166,11 +165,11 @@ class CI_Cache_file extends CI_Driver {
 			}
 
 			return array(
-				'expire' 	=> $mtime + $data['ttl'],
+				'expire'	=> $mtime + $data['ttl'],
 				'mtime'		=> $mtime
 			);
 		}
-		
+
 		return FALSE;
 	}
 
