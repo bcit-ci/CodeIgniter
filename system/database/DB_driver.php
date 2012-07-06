49aa45b45e1cc83cb61d1524ba32d6c188dac2e1
Andrey Andreev <narf@bofh.bg>
2012-07-06 16:22:21 +0300

    Fix a few join() bugs

--- system/database/DB_driver.php
+++ system/database/DB_driver.php
@@ -970,7 +970,7 @@ abstract class CI_DB_driver {
 	 */
 	public function escape_identifiers($item)
 	{
-		if ($this->_escape_char === '')
+		if ($this->_escape_char === '' OR empty($item))
 		{
 			return $item;
 		}
@@ -983,8 +983,8 @@ abstract class CI_DB_driver {
 
 			return $item;
 		}
-		// Avoid breaking functions inside queries
-		elseif (strpos($item, '(') !== FALSE)
+		// Avoid breaking functions and literal values inside queries
+		elseif (ctype_digit($item) OR $item[0] === "'" OR strpos($item, '(') !== FALSE)
 		{
 			return $item;
 		}
