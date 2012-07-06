52819826e3ecbc4a0f0eba84c2693590ffcdcf4a
Gabriele Antonini <gabriele.antonini@gmail.com>
2012-07-06 11:28:18 +0200

    Adding sql query explain info in profiler

--- system/language/english/profiler_lang.php
+++ system/language/english/profiler_lang.php
@@ -45,6 +45,8 @@ $lang['profiler_no_memory']		= 'Memory Usage Unavailable';
 $lang['profiler_no_profiles']	= 'No Profile data - all Profiler sections have been disabled.';
 $lang['profiler_section_hide']	= 'Hide';
 $lang['profiler_section_show']	= 'Show';
+$lang['profiler_explain_hide']	= 'Hide explain';
+$lang['profiler_explain_show']	= 'Show explain';
 
 /* End of file profiler_lang.php */
 /* Location: ./system/language/english/profiler_lang.php */
