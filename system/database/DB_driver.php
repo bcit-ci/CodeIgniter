5806473b6f002d4ae637844f38afa3c08b1cb610
Andrey Andreev <narf@bofh.bg>
2012-10-09 10:36:04 +0300

    Fix issue #1862

--- system/database/DB_driver.php
+++ system/database/DB_driver.php
@@ -1363,7 +1363,7 @@ abstract class CI_DB_driver {
 				$call['file'] = str_replace('\\', '/', $call['file']);
 			}
 
-			if (isset($call['file'], $call['class']) && strpos($call['file'], $basepath.'database') === FALSE && strpos($call['class'], 'Loader') !== FALSE)
+			if (isset($call['file'], $call['class']) && strpos($call['file'], BASEPATH.'database') === FALSE && strpos($call['class'], 'Loader') !== FALSE)
 			{
 				// Found it - use a relative path for safety
 				$message[] = 'Filename: '.str_replace(array(APPPATH, BASEPATH), '', $call['file']);
