f795ab52dadaef20afd3a97ad4c8ed408e211dc2
Andrey Andreev <narf@bofh.bg>
2012-10-24 21:28:25 +0300

    [ci skip] Document get_csrf_token_name(), get_csrf_hash() (issue #715)

--- system/core/Security.php
+++ system/core/Security.php
@@ -161,7 +161,7 @@ class CI_Security {
 		}
 
 		// Do the tokens exist in both the _POST and _COOKIE arrays?
-		if ( ! isset($_POST[$this->_csrf_token_name]) OR ! isset($_COOKIE[$this->_csrf_cookie_name])
+		if ( ! isset($_POST[$this->_csrf_token_name], $_COOKIE[$this->_csrf_cookie_name])
 			OR $_POST[$this->_csrf_token_name] !== $_COOKIE[$this->_csrf_cookie_name]) // Do the tokens match?
 		{
 			$this->csrf_show_error();
