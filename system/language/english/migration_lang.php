34c8b9c45fdcd2eb0eee5e2275a52e4c2faed5dc
Jonathon Hill <jhill@brandmovers.com>
2012-10-31 14:02:35 -0400

    Added support for timestamp-based migrations
    Signed-off-by: Jonathon Hill <jhill@brandmovers.com>

--- system/language/english/migration_lang.php
+++ system/language/english/migration_lang.php
@@ -27,6 +27,7 @@
 
 $lang['migration_none_found']			= 'No migrations were found.';
 $lang['migration_not_found']			= 'No migration could be found with the version number: %d.';
+$lang['migration_sequence_gap']         = 'There is a gap in the migration sequence near version number: %d.';
 $lang['migration_multiple_version']		= 'There are multiple migrations with the same version number: %d.';
 $lang['migration_class_doesnt_exist']	= 'The migration class "%s" could not be found.';
 $lang['migration_missing_up_method']	= 'The migration class "%s" is missing an "up" method.';
