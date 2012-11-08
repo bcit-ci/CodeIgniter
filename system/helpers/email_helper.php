6594eda7a27cb0bfc7a7b6a80e402d6807c48921
Andrey Andreev <narf@bofh.bg>
2012-11-08 15:13:52 +0200

    Deprecate the Email helper

--- system/helpers/email_helper.php
+++ system/helpers/email_helper.php
@@ -43,7 +43,8 @@ if ( ! function_exists('valid_email'))
 	/**
 	 * Validate email address
 	 *
-	 * @param	string
+	 * @deprecated	3.0.0	Use PHP's filter_var() instead
+	 * @param	string	$email
 	 * @return	bool
 	 */
 	function valid_email($email)
@@ -59,12 +60,13 @@ if ( ! function_exists('send_email'))
 	/**
 	 * Send an email
 	 *
-	 * @param	string
-	 * @param	string
-	 * @param	string
+	 * @deprecated	3.0.0	Use PHP's mail() instead
+	 * @param	string	$recipient
+	 * @param	string	$subject
+	 * @param	string	$message
 	 * @return	bool
 	 */
-	function send_email($recipient, $subject = 'Test email', $message = 'Hello World')
+	function send_email($recipient, $subject, $message)
 	{
 		return mail($recipient, $subject, $message);
 	}
