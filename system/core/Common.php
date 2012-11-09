0aeee85b5164e31e2a76c05caa97bd898c1a776d
Andrey Andreev <narf@bofh.bg>
2012-11-09 17:43:11 +0200

    Move the Log class to system/core/
    It is a core class after all, I guess somebody forgot it when the rest of them were moved.

--- system/core/Common.php
+++ system/core/Common.php
@@ -420,7 +420,7 @@ if ( ! function_exists('log_message'))
 			return;
 		}
 
-		$_log =& load_class('Log');
+		$_log =& load_class('Log', 'core');
 		$_log->write_log($level, $message, $php_error);
 	}
 }
