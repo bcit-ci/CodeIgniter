1713d322a309d9cf405cfe83e2131368f29b222b
dchill42 <dchill42@gmail.com>
2012-11-24 20:23:21 -0500

    Replaced Mock_Core_Lang with PHPUnit mockups
    Signed-off-by: dchill42 <dchill42@gmail.com>

--- tests/codeigniter/helpers/language_helper_test.php
+++ tests/codeigniter/helpers/language_helper_test.php
@@ -5,7 +5,9 @@ class Language_helper_test extends CI_TestCase {
 	public function test_lang()
 	{
 		$this->helper('language');
-		$this->ci_instance_var('lang', new Mock_Core_Lang());
+		$lang = $this->getMock('CI_Lang', array('line'));
+		$lang->expects($this->any())->method('line')->will($this->returnValue(FALSE));
+		$this->ci_instance_var('lang', $lang);
 
 		$this->assertFalse(lang(1));
 		$this->assertEquals('<label for="foo"></label>', lang(1, 'foo'));
