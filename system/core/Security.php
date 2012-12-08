3460de8740fcd35432ccb82a6f68b3672ac586bf
brian978 <dbrian89@yahoo.com>
2012-12-08 23:22:26 +0200

    All the HEX code must be replaced or else some XSS attacks can be successful

--- system/core/Security.php
+++ system/core/Security.php
@@ -526,9 +526,17 @@ class CI_Security {
 			$charset = config_item('charset');
 		}
 
-		$str = html_entity_decode($str, ENT_COMPAT, $charset);
-		$str = preg_replace('~&#x(0*[0-9a-f]{2,5})~ei', 'chr(hexdec("\\1"))', $str);
-		return preg_replace('~&#([0-9]{2,4})~e', 'chr(\\1)', $str);
+                do
+                {
+                    $matches = $matches1 = 0;
+                    
+                    $str = html_entity_decode($str, ENT_COMPAT, $charset);
+                    $str = preg_replace('~&#x(0*[0-9a-f]{2,5})~ei', 'chr(hexdec("\\1"))', $str, -1, $matches);
+                    $str = preg_replace('~&#([0-9]{2,4})~e', 'chr(\\1)', $str, -1, $matches1);
+                }
+                while($matches || $matches1);
+
+                return $str;
 	}
 
 	// --------------------------------------------------------------------
