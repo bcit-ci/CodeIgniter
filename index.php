e90b8305e6fec8b7935fbbbe24f68a62332df886
Adam Carmichael <carneeki@carneeki.net>
2013-01-04 01:04:29 +1100

    fix a typo in index.php

--- index.php
+++ index.php
@@ -87,7 +87,7 @@ switch (ENVIRONMENT)
  *---------------------------------------------------------------
  *
  * If you want this front controller to use a different "application"
- * folder then the default one you can set its name here. The folder
+ * folder than the default one you can set its name here. The folder
  * can also be renamed or relocated anywhere on your server. If
  * you do, use a full server path. For more info please see the user guide:
  * http://codeigniter.com/user_guide/general/managing_apps.html
