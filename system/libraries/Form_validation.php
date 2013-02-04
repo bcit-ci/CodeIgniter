ea17ca2e81ce589b092a4dce832dd65337d0ec95
Shannon Brooks <shannon@brooksworks.com>
2013-02-04 11:58:59 -0600

    Update system/libraries/Form_validation.php
    Fix issue with RURI not matching for router/method/param, per documentation.

--- system/libraries/Form_validation.php
+++ system/libraries/Form_validation.php
@@ -414,8 +414,8 @@ class CI_Form_validation {
 				return FALSE;
 			}
 
-			// Is there a validation rule for the particular URI being accessed?
-			$uri = ($group === '') ? trim($this->CI->uri->ruri_string(), '/') : $group;
+			// Is there a validation rule for the particular Controller/Method being accessed?
+			$uri = ($group == '') ? $this->CI->router->fetch_class().'/'.$this->CI->router->fetch_method() : $group;
 
 			if ($uri !== '' && isset($this->_config_rules[$uri]))
 			{
