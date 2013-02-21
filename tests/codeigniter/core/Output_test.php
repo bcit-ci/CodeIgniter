eb291c1d1e1116a4420fa30e587adeea0451eeb7
Andrey Andreev <narf@bofh.bg>
2013-02-21 22:22:51 +0200

    CI_Output [set/append/get]_output() tests

--- tests/codeigniter/core/Output_test.php
+++ tests/codeigniter/core/Output_test.php
@@ -3,12 +3,47 @@
 class Output_test extends CI_TestCase {
 
 	public $output;
+	protected $_output_data = <<<HTML
+<html>
+	<head>
+		<title>Basic HTML</title>
+	</head>
+	<body>
+		Test
+	</body>
+</html>
+HTML;
 
 	public function set_up()
 	{
 		$this->ci_set_config('charset', 'UTF-8');
 		$output = $this->ci_core_class('output');
 		$this->output = new $output();
+	}
+
+	// --------------------------------------------------------------------
+
+	public function test_set_get_append_output()
+	{
+		$append = "<!-- comment /-->\n";
+
+		$this->assertEquals(
+			$this->_output_data.$append,
+			$this->output
+				->set_output($this->_output_data)
+				->append_output("<!-- comment /-->\n")
+				->get_output()
+		);
+	}
+
+	// --------------------------------------------------------------------
+
+	public function test_minify()
+	{
+		$this->assertEquals(
+			str_replace(array("\t", "\n"), '', $this->_output_data),
+			$this->output->minify($this->_output_data)
+		);
 	}
 
 	// --------------------------------------------------------------------
