010169df4331add24845f17e45bd5d4327b80e1c
RJ garcia <rj@bighead.net>
2013-03-08 00:11:57 -0800

    Updating cache driver to be like session driver Signed-off-by: RJ garcia <rj@bighead.net>

--- application/config/config.php
+++ application/config/config.php
@@ -251,17 +251,6 @@ $config['log_date_format'] = 'Y-m-d H:i:s';
 
 /*
 |--------------------------------------------------------------------------
-| Cache Directory Path
-|--------------------------------------------------------------------------
-|
-| Leave this BLANK unless you would like to set something other than the default
-| application/cache/ folder.  Use a full server path with trailing slash.
-|
-*/
-$config['cache_path'] = '';
-
-/*
-|--------------------------------------------------------------------------
 | Encryption Key
 |--------------------------------------------------------------------------
 |
@@ -324,6 +313,34 @@ $config['cookie_domain']	= '';
 $config['cookie_path']		= '/';
 $config['cookie_secure']	= FALSE;
 $config['cookie_httponly'] 	= FALSE;
+
+/*
+|--------------------------------------------------------------------------
+| Cache Variables
+|--------------------------------------------------------------------------
+|
+| 'cache_adapter'		= the adapter (driver) to load: apc, dummy, file, memcached, redis, wincache,
+|	or your custom driver name
+| 'cache_backup_driver'	= the backup driver to load up: same values accepted as cache_driver
+| 'cache_valid_drivers'	= additional valid drivers which may be loaded
+| 'cache_key_prefix'	= a specific prefix for you cache keys
+*/
+$config['cache_driver']			= 'dummy';
+$config['cache_backup_driver']	= 'dummy';
+$config['cache_valid_drivers']	= array();
+$config['cache_key_prefix']		= '';
+
+/*
+|--------------------------------------------------------------------------
+| Cache Directory Path
+|--------------------------------------------------------------------------
+|
+| Leave this BLANK unless you would like to set something other than the default
+| application/cache/ folder.  Use a full server path with trailing slash.
+| Only applies to the CI_Cache_file driver
+|
+*/
+$config['cache_path'] = '';
 
 /*
 |--------------------------------------------------------------------------
--- system/libraries/Cache/Cache.php
+++ system/libraries/Cache/Cache.php
@@ -52,13 +52,6 @@ class CI_Cache extends CI_Driver_Library {
 	);
 
 	/**
-	 * Path of cache files (if file-based cache)
-	 *
-	 * @var string
-	 */
-	protected $_cache_path = NULL;
-
-	/**
 	 * Reference to the driver
 	 *
 	 * @var mixed
@@ -87,30 +80,48 @@ class CI_Cache extends CI_Driver_Library {
 	 * @param	array	$config = array()
 	 * @return	void
 	 */
-	public function __construct($config = array())
+	public function __construct(array $config = array())
 	{
-		$default_config = array(
-			'adapter',
-			'memcached'
-		);
-
-		foreach ($default_config as $key)
+		$CI =& get_instance();
+		
+		/*
+		 * A list of config options that we need to check for
+		 * 1. Adapter (adapter)
+		 * 2. Backup driver (backup)
+		 * 3. Key prefix (key_prefix)
+		 */
+		
+		$adapter = isset($config['adapter']) ? $config['adapter'] : $CI->config->item('cache_driver');
+		
+		// if $adapter returns a TRUE value, then set it to $this->_adapter
+		if ($adapter)
 		{
-			if (isset($config[$key]))
-			{
-				$param = '_'.$key;
-
-				$this->{$param} = $config[$key];
-			}
+			$this->_adapter = $adapter;
 		}
-
-		isset($config['key_prefix']) && $this->key_prefix = $config['key_prefix'];
-
-		if (isset($config['backup']) && in_array('cache_'.$config['backup'], $this->valid_drivers))
+		 
+		$backup = isset($config['backup']) ? $config['backup'] : $CI->config->item('cache_backup_driver');
+		if ($backup)
 		{
-			$this->_backup_driver = $config['backup'];
+			$this->_backup_driver = $backup;
 		}
-
+		
+		$drivers = isset($config['valid_drivers']) ? $config['valid_drivers'] : $CI->config->item('cache_valid_drivers');
+		if (is_array($drivers))
+		{
+			/*
+			 * no need to do any extra processing to make sure user didn't input driver names already
+			 * in the valid drivers array or if the user inserted duplicates because it won't hurt
+			 * anything. Let's not do any busy work.
+			 */
+			$this->valid_drivers = array_merge($this->valid_drivers, $drivers);
+		}
+		
+		$key_prefix = isset($config['key_prefix']) ? $config['key_prefix'] : $CI->config->item('cache_key_prefix');
+		if (is_string($key_prefix))
+		{
+			$this->key_prefix = $key_prefix;
+		}
+		
 		// If the specified adapter isn't available, check the backup.
 		if ( ! $this->is_supported($this->_adapter))
 		{
@@ -229,7 +240,174 @@ class CI_Cache extends CI_Driver_Library {
 
 		return $support[$driver];
 	}
+	
+	/**
+	 * Magic method to allow drivers to support more methods than 
+	 * the default
+	 *
+	 * @param	string	$method method to call
+	 * @param	string	$args array of args
+	 */
+	public function __call($method, $args)
+	{
+		if (method_exists($this->{$this->_adapter}, $property))
+		{
+			return call_user_func(array($this->{$this->_adapter}, $method), $args);
+		}
+		
+		return NULL;
+	}
 
+}
+
+/**
+ * CI_Cache_driver Class
+ *
+ * Extend this class to make a new CI_Cache driver.
+ * A CI_Cache_driver implements key/value caching that persists across requests.
+ * To make a new driver, derive from (extend) CI_Cache_driver. Overload the initialize method to simulate a contructor.
+ * Then just implement the abstract functions in a similar fashion to the other CI Caching drivers.
+ * Put your driver in the libraries/Cache/drivers folder anywhere in the loader paths. This includes the
+ * application directory, the system directory, or any path you add with $CI->load->add_package_path().
+ * Your driver must be named CI_Cache_<name>, and your filename must be Cache_<name>.php,
+ * preferably also capitalized. (e.g.: CI_Cache_foo in libraries/Cache/drivers/Cache_foo.php)
+ * Then specify the driver by setting 'cache_driver' in your config file or as a parameter when loading the CI_Cache
+ * object. (e.g.: $config['cache_driver'] = 'foo'; OR $CI->load->driver('cache', array('cache_driver' => 'foo')); )
+ *
+ * @package		CodeIgniter
+ * @subpackage	Libraries
+ * @category	Sessions
+ * @author		EllisLab Dev Team
+ */
+abstract class CI_Cache_driver extends CI_Driver {
+
+	/**
+	 * CI Singleton
+	 *
+	 * @see	get_instance()
+	 * @var	object
+	 */
+	protected $CI;
+
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Constructor
+	 *
+	 * Gets the CI singleton, so that individual drivers
+	 * don't have to do it separately.
+	 *
+	 * @return	void
+	 */
+	public function __construct()
+	{
+		$this->CI =& get_instance();
+	}
+	
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Decorate
+	 *
+	 * Decorates the child with the parent driver lib's methods and properties
+	 *
+	 * @param	object	Parent library object
+	 * @return	void
+	 */
+	public function decorate($parent)
+	{
+		// Call base class decorate first
+		parent::decorate($parent);
+
+		// Call initialize method now that driver has access to $this->_parent
+		$this->initialize();
+	}
+
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Initialize driver
+	 *
+	 * @return	void
+	 */
+	protected function initialize()
+	{
+		// Overload this method to implement initialization
+	}
+	
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Get
+	 *
+	 * Look for a value in the cache. If it exists, return the data
+	 * if not, return FALSE
+	 *
+	 * @param	string	$id
+	 * @return	mixed	value matching $id or FALSE on failure
+	 */
+	abstract public function get($id);
+
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Cache Save
+	 *
+	 * @param	string	$id		Cache ID
+	 * @param	mixed	$data		Data to store
+	 * @param	int	$ttl = 60	Cache TTL (in seconds)
+	 * @return	bool	TRUE on success, FALSE on failure
+	 */
+	abstract public function save($id, $data, $ttl = 60);
+
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Delete from Cache
+	 *
+	 * @param	string	$id	Cache ID
+	 * @return	bool	TRUE on success, FALSE on failure
+	 */
+	abstract public function delete($id);
+
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Clean the cache
+	 *
+	 * @return	bool	TRUE on success, FALSE on failure
+	 */
+	abstract public function clean();
+
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Cache Info
+	 *
+	 * @param	string	$type = 'user'	user/filehits
+	 * @return	mixed	array containing cache info on success OR FALSE on failure
+	 */
+	abstract public function cache_info($type = 'user');
+	
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Get Cache Metadata
+	 *
+	 * @param	string	$id	key to get cache metadata on
+	 * @return	mixed	cache item metadata
+	 */
+	abstract public function get_metadata($id);
+
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Is the requested driver supported in this environment?
+	 *
+	 * @param	string	$driver	The driver to test
+	 * @return	array
+	 */
+	abstract public function is_supported($driver);
 }
 
 /* End of file Cache.php */
