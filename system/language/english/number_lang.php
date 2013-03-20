a5792896db1f1e5d0d24904257be1edd0146eb1d
groovenectar <contact@danielupshaw.com>
2013-03-20 13:00:24 -0300

    Changed KB to kB
    Per http://en.wikipedia.org/wiki/Kilobyte

--- system/language/english/number_lang.php
+++ system/language/english/number_lang.php
@@ -29,7 +29,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $lang['terabyte_abbr'] = 'TB';
 $lang['gigabyte_abbr'] = 'GB';
 $lang['megabyte_abbr'] = 'MB';
-$lang['kilobyte_abbr'] = 'KB';
+$lang['kilobyte_abbr'] = 'kB';
 $lang['bytes'] = 'Bytes';
 
 /* End of file number_lang.php */
