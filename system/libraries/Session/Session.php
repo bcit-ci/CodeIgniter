6123ce2f061bfb7591477c8804436c7bd724863f
Kapitanluffy Pirata <kapitanluffy@gmail.com>
2013-06-03 16:37:39 +0800

    Update Session library - convert \r characters to ""
    Putting \r characters to flashdata causes hash_hmac() under _sess_read() to produce a different hash. Fixed by replacing \r characters to "" in set_userdata()

--- system/libraries/Session/Session.php
+++ system/libraries/Session/Session.php
@@ -304,6 +304,7 @@ class CI_Session extends CI_Driver_Library {
 		{
 			foreach ($newdata as $key => $val)
 			{
+				$val = str_replace("\r", "", $val);
 				$this->userdata[$key] = $val;
 			}
 		}
