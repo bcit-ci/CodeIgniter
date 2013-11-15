05e0ddec40fba085cfe414302536f41c0a06bfcc
Josh Sephton <josh@explodingphone.com>
2013-11-15 12:55:43 +0000

    Added .htaccess to remove 'index.php' from path.

--- application/config/config.php
+++ application/config/config.php
@@ -26,7 +26,7 @@ $config['base_url']	= '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
