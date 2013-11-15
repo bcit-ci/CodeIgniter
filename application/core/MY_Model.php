4ff0266f3774b9221e5476dbda1f8c755db11265
Josh Sephton <josh@explodingphone.com>
2013-11-15 12:54:23 +0000

    Added a model superclass which provides object to database mapping. Added a relationship class which allows relationships between models.

---
+++ application/core/MY_Model.php
@@ -0,0 +1,262 @@
+<?php
+
+class MY_Model extends CI_Model
+{
+	protected $database_table_name;
+	protected $database_keys;
+	
+	protected $relationships;
+	
+	protected $hidden_properties;
+	
+	public $updated;
+	public $deleted;
+	
+	public function __construct(){
+		parent::__construct();
+		
+		$this->load->model('my_model_relationship');
+		
+		$this->database_table_name = static::database_table_name();
+		$this->database_keys = static::database_keys();
+		
+		$this->relationships = static::relationships();
+		
+		$this->hidden_properties = static::hidden_properties();
+		
+		$this->deleted = 0;
+	}
+	
+	public static function database_table_name()
+	{
+		return '';
+	}
+	
+	public static function database_keys()
+	{
+		return array();
+	}
+	
+	public static function relationships()
+	{
+		return array();
+	}
+	
+	public static function hidden_properties()
+	{
+		return array();
+	}
+	
+	public static function create()
+	{
+		return new static;
+	}
+	
+	public static function get($filters = NULL, $modified_since = NULL, $exclude_deleted = TRUE)
+	{
+		$CI =& get_instance();
+		if($filters)
+		{
+			foreach(static::database_keys() as $k)
+			{
+				$CI->db->select($k);
+			}
+			$CI->db->select('updated');
+			if(!$exclude_deleted)
+			{
+				$CI->db->select('deleted');
+			}
+			$CI->db->from($CI->database_table_name);
+			foreach($filters as $value)
+			{
+				$CI->db->where($value);
+			}
+			if($exclude_deleted)
+			{
+				$CI->db->where(static::database_table_name().'.deleted', '0');
+			}
+			if($modified_since)
+			{
+				$CI->db->where(static::database_table_name().'.updated >=', $modified_since);
+			}
+			$q = $CI->db->get();
+			if ($q->num_rows() === 1)
+			{
+				$x = new static;
+				foreach($q->result() as $object)
+				{	
+					foreach($object as $k => $v)
+					{
+						$x->$k = $v;
+					}
+				}
+				return $x;
+			}
+		}
+		return NULL;
+	}
+	
+	public static function get_many($filters = NULL, $modified_since = NULL, $limit = 0, $offset = 0, $exclude_deleted = TRUE){
+		$CI =& get_instance();
+		$objects = array();
+		foreach(static::database_keys() as $k)
+		{
+			$CI->db->select($k);
+		}
+		$CI->db->select('updated');
+		if(!$exclude_deleted)
+		{
+			$CI->db->select('deleted');
+		}
+		$CI->db->from(static::database_table_name());
+		if($filters)
+		{
+			foreach($filters as $value)
+			{
+				$CI->db->where($value);
+			}
+		}
+		if($exclude_deleted)
+		{
+			$CI->db->where(static::database_table_name().'.deleted', '0');
+		}
+		if($modified_since)
+		{
+			$CI->db->where(static::database_table_name().'.updated >=', $modified_since);
+		}
+		if($limit)
+		{
+			if($offset)
+			{
+				$CI->db->limit($limit, $offset);
+			} 
+			else 
+			{
+				$CI->db->limit($limit);
+			}
+		}
+		$q = $CI->db->get();
+		if ($q->num_rows() > 0)
+		{
+			foreach($q->result() as $object)
+			{
+				$x = new static;
+				foreach($object as $k => $v)
+				{
+					$x->$k = $v;
+				}
+				$objects[] = $x;
+			}
+		}
+		return $objects;
+	}
+	
+	public function populate($indata)
+	{
+		if(is_array($indata))
+		{
+			foreach ($this->database_keys as $key) 
+			{
+				if(isset($indata[$key]))
+				{
+					$this->$key = $indata[$key];
+				}
+			}
+		} 
+		else if(is_object($indata))
+		{
+			foreach ($this->database_keys as $key) 
+			{
+				if(isset($indata->$key))
+				{
+					$this->$key = $indata->$key;
+				}
+			}
+		}
+	}
+	
+	public function save()
+	{
+		$this->updated = date("Y-m-d H:i:s");
+		if($this->id)
+		{
+			$this->db->where('id', $this->id);
+			$this->db->update($this->database_table_name, $this);
+		} 
+		else 
+		{
+			$this->db->insert($this->database_table_name, $this);
+			$this->id = $this->db->insert_id();
+		}
+	}
+	
+	public function delete()
+	{
+		$this->deleted = 1;
+		$this->save();
+	}
+	
+	public function create_relationship($relationship_key, $object)
+	{
+		if(isset($this->relationships[$relationship_key])){
+			$relationship_model = static::_relationship_class($this, $this->relationships[$relationship_key]);
+			$this->load->model($relationship_model);
+			$relationship::create($this, $object);
+		}
+	}
+	
+	public function remove_relationship($relationship_key, $object)
+	{
+		if(isset($this->relationships[$relationship_key])){
+			$relationship_model = static::_relationship_class($this, $this->relationships[$relationship_key]);
+			$this->load->model($relationship_model);
+			$relationship::delete($this, $object);
+		}
+	}
+	
+	public function hide_properties()
+	{
+		foreach ($this->hidden_properties as $property)
+		{
+			if(isset($this->$property))
+			{
+				unset($this->$property);
+			}
+		}
+	}
+	
+	function __get($key)
+	{
+		$relationship_key = $key;
+		$is_count = FALSE;
+		if (strpos($str, "count_") === 0)
+		{
+			$relationship_key = substr($str, strlen('count_'));
+			$is_count = TRUE;
+		}
+		
+		if(in_array($relationship_key, $this->relationships))
+		{
+			$relationship_model = static::_relationship_class($this, $this->relationships[$relationship_key]);
+			$this->load->model($relationship_model);
+			if(!$is_count)
+			{
+				$this->$key = $relationship::get_related($this, $filters, $modified_since, $exclude_deleted);
+			}
+			else
+			{
+				$this->$key = $relationship::count_related($this, $filters, $modified_since, $exclude_deleted);
+			}
+			return $this->$key;
+		}
+		else 
+		{
+			parent::__get($key);
+		}
+	}
+	
+	private static function _relationship_class($left, $right)
+	{
+		return 'relationships/' . tolower(get_class($left)) . tolower($right);
+	}
+}
---
+++ application/core/MY_Model_Relationship.php
@@ -0,0 +1,209 @@
+<?php
+
+class MY_Model_Relationship 
+{
+	protected $database_table_name;
+	protected $database_key_left_to_center;
+	protected $database_key_center_to_left;
+	protected $database_key_right_to_center;
+	protected $database_key_center_to_right;
+	
+	protected $model_left;
+	protected $model_right;
+	
+	protected $to_many;
+	
+	public static function database_table_name()
+	{
+		return '';
+	}
+	
+	public static function database_key_left_to_center()
+	{
+		return '';
+	}
+	
+	public static function database_key_center_to_left()
+	{
+		return '';
+	}
+	
+	public static function database_key_right_to_center()
+	{
+		return '';
+	}
+	
+	public static function database_key_center_to_right()
+	{
+		return '';
+	}
+	
+	public static function left_model()
+	{
+		return '';
+	}
+	
+	public static function right_model()
+	{
+		return '';
+	}
+	
+	public static function to_many()
+	{
+		return 0;
+	}
+	
+	public static function create($left_object, $right_object)
+	{
+		$CI =& get_instance();
+		
+		$CI->load->model(static::model_left());	
+		$CI->load->model(static::model_right());
+		
+		$database_key_left_to_center = static::database_key_left_to_center();
+		$database_key_right_to_center = static::database_key_right_to_center();
+		
+		$left_key = $left_object->$database_key_left_to_center;
+		$right_key = $right_object->$database_key_right_to_center;
+		
+		$indata = array();
+		$indata['updated'] = date("Y-m-d H:i:s");
+		$indata['deleted'] = '0';
+		
+		$CI->db->where(static::database_key_center_to_left(), $left_key);
+		$CI->db->where(static::database_key_center_to_right(), $right_key);
+	    if ($CI->db->count_all_results(static::database_table_name()) == 0) 
+	    {
+	    	$indata[static::database_key_center_to_left()] = $left_key;
+			$indata[static::database_key_center_to_right()] = $right_key;	
+	    	$CI->db->insert(static::database_table_name(), $indata);
+	    } 
+	    else 
+	    {
+	    	$CI->db->where(static::database_key_center_to_left(), $left_key);
+			$CI->db->where(static::database_key_center_to_right(), $right_key);
+	    	$CI->db->update(static::database_table_name(), $indata);
+	    }
+	}
+
+
+	public static function delete($left_object, $right_object)
+	{
+		$CI =& get_instance();
+		
+		$CI->load->model(static::model_left());	
+		$CI->load->model(static::model_right());
+		
+		$database_key_left_to_center = static::database_key_left_to_center();
+		$database_key_right_to_center = static::database_key_right_to_center();
+		
+		$left_key = $left_object->$database_key_left_to_center;
+		$right_key = $right_object->$database_key_right_to_center;
+		
+		$indata = array();
+		$indata['updated'] = date("Y-m-d H:i:s");
+		$indata['deleted'] = '1';
+		
+		$CI->db->where(static::database_key_center_to_left(), $left_key);
+		$CI->db->where(static::database_key_center_to_right(), $right_key);
+	    if ($CI->db->count_all_results(static::database_table_name()) > 0)
+	    {
+	    	$CI->db->where(static::database_key_center_to_left(), $left_key);
+			$CI->db->where(static::database_key_center_to_right(), $right_key);
+	    	$CI->db->update(static::database_table_name(), $indata);
+	    }
+	}
+	
+	public static function count_related($left_object, $filters = NULL, $modified_since = NULL, $exclude_deleted = TRUE){
+		return static::get($left_object, $filters, $modified_since, $exclude_deleted, TRUE);
+	}
+	
+	public static function get_related($left_object, $filters = NULL, $modified_since = NULL, $exclude_deleted = TRUE, $count_only = FALSE)
+	{
+		$CI =& get_instance();
+			
+		$CI->load->model(static::left_model());	
+		$CI->load->model(static::right_model());
+		
+		$model_left = static::left_model();
+		$model_right = static::right_model();
+		
+		$left_table = $model_left::GetDatabaseTableName();
+		$center_table = static::GetDatabaseTableName();
+		$right_table = $model_right::GetDatabaseTableName();
+		
+		$database_key_left_to_center = static::database_key_left_to_center();
+		$left_key = $left_object->$database_key_left_to_center;
+		
+		foreach($model_right::GetDatabaseKeys() as $key)
+		{
+			$CI->db->select($right_table.'.'.$key);
+		}
+		$CI->db->select($right_table.'.updated');
+		if(!$exclude_deleted){
+			$CI->db->select($right_table.'.deleted');
+		}
+		$CI->db->from($left_table);
+		$CI->db->join($center_table, $left_table.'.'.static::database_key_left_to_center().' = '.$center_table.'.'.static::database_key_center_to_left());
+		$CI->db->join($right_table, $center_table.'.'.static::database_key_center_to_right().' = '.$right_table.'.'.static::database_key_right_to_center());
+		$CI->db->where($left_table.'.'.static::database_key_left_to_center(), $left_key);
+		$CI->db->where($center_table.'.deleted', '0');
+		if($filters)
+		{
+			foreach($filters as $value){
+				$this->db->where($value);
+			}
+		}
+		if($exclude_deleted)
+		{
+			$CI->db->where($right_table.'.deleted', '0');
+		}
+		if($modified_since)
+		{
+			$CI->db->where($center_table.'.updated >=', $modified_since);
+		}
+		
+		if(!$count_only)
+		{
+			if($this->to_many)
+			{
+				$q = $CI->db->get();
+				$results = array();
+				if ($q->num_rows() > 0)
+				{
+					foreach($q->result() as $object)
+					{
+						$x = new $model_right;
+						foreach($object as $key => $value)
+						{
+							$x->$key = $value;
+						}
+						$results[] = $x;
+					}
+				}
+				return $results;
+			} 
+			else 
+			{
+				$q = $CI->db->get();
+				if ($q->num_rows() === 1)
+				{
+					$x = new $model_right;
+					foreach($q->result() as $object)
+					{		
+						foreach($object as $key => $value)
+						{
+							$x->$key = $value;
+						}
+					}
+					return $x;
+				}
+				return NULL;
+			}
+		} 
+		else 
+		{
+			return $CI->db->count_all_results();
+		}
+	}
+}
