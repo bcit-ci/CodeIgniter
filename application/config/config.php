598c7eccf43a9e56547cd3eb45b2bc25c459847c
darwinel <kmorssink@gmail.com>
2014-02-09 15:37:27 +0100

    change back to original

--- application/config/config.php
+++ application/config/config.php
@@ -302,12 +302,12 @@ $config['sess_driver']			= 'cookie';
 $config['sess_valid_drivers']	= array();
 $config['sess_cookie_name']		= 'ci_session';
 $config['sess_expiration']		= 7200;
-$config['sess_expire_on_close']	= TRUE;
-$config['sess_encrypt_cookie']	= TRUE;
+$config['sess_expire_on_close']	= FALSE;
+$config['sess_encrypt_cookie']	= FALSE;
 $config['sess_use_database']	= FALSE;
 $config['sess_table_name']		= 'ci_sessions';
-$config['sess_match_ip']		= TRUE;
-$config['sess_match_useragent']	= TRUE;
+$config['sess_match_ip']		= FALSE;
+$config['sess_match_useragent']	= FALSE;
 $config['sess_time_to_update']	= 300;
 
 /*
