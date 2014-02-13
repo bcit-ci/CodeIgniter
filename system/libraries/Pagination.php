526ded927a607b4dfab0ec00f1d52efa3df0d887
Andrey Andreev <narf@devilix.net>
2014-02-13 16:29:52 +0200

    Fix #2878

--- system/libraries/Pagination.php
+++ system/libraries/Pagination.php
@@ -305,7 +305,7 @@ class CI_Pagination {
 	public function __construct($params = array())
 	{
 		$CI =& get_instance();
-		$CI->load->language('paginaton');
+		$CI->load->language('pagination');
 		foreach (array('first_link', 'next_link', 'prev_link', 'last_link') as $key)
 		{
 			if (($val = $CI->lang->line('pagination_'.$key)) !== FALSE)
