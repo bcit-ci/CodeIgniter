1fcc023a0a5205739cfc7255688f588bcff1b2da
Ahmad Anbar <aanbar@gmail.com>
2014-02-20 00:48:07 +0200

    Supress ini_set errors.

--- index.php
+++ index.php
@@ -56,13 +56,13 @@ switch (ENVIRONMENT)
 {
 	case 'development':
 		error_reporting(-1);
-		ini_set('display_errors', 1);
+		@ini_set('display_errors', 1);
 	break;
 
 	case 'testing':
 	case 'production':
 		error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT);
-		ini_set('display_errors', 0);
+		@ini_set('display_errors', 0);
 	break;
 
 	default:
