d3ddf9763aba68eed1bce7dde275b9be253efcc0
mdunisch <subseven>
2014-05-07 15:00:40 +0200

    Remove error-masking '@' in mysqli_driver (See: https://github.com/EllisLab/CodeIgniter/issues/3036)

--- system/database/drivers/mysqli/mysqli_driver.php
+++ system/database/drivers/mysqli/mysqli_driver.php
@@ -144,7 +144,7 @@ class CI_DB_mysqli_driver extends CI_DB {
 			$database = $this->database;
 		}
 
-		if (@$this->conn_id->select_db($database))
+		if ($this->conn_id->select_db($database))
 		{
 			$this->database = $database;
 			return TRUE;
@@ -197,7 +197,7 @@ class CI_DB_mysqli_driver extends CI_DB {
 	 */
 	protected function _execute($sql)
 	{
-		return @$this->conn_id->query($this->_prep_query($sql));
+		return $this->conn_id->query($this->_prep_query($sql));
 	}
 
 	// --------------------------------------------------------------------
