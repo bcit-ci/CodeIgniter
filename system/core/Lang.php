541f941fa3370a49579e4a3f8989007074a8fa4b
Dimitar Mihaylov <dimitar@smartlytaken.com>
2014-09-08 21:42:13 +0300

    Updated is Lang.php according to recent comments in Github

--- system/core/Lang.php
+++ system/core/Lang.php
@@ -162,20 +162,14 @@ class CI_Lang {
 	 *
 	 * Fetches a single line of text from the language array
 	 *
-	 * @param	 string	$line				Language line key
-	 * @param  mixed 	$args 			Variables to be parsed within string
-	 * @param	 bool		$log_errors	Whether to log an error message if the line is not found
-	 * @return string							Translation
+	 * @param	string	$line		Language line key
+	 * @param array $args Variables to be parsed within string
+	 * @param	bool	$log_errors	Whether to log an error message if the line is not found
+	 * @return	string	Translation
 	 */
-	public function line($line = '', $args = '', $log_errors = TRUE)
+	public function line($line, array $args = NULL, $log_errors = TRUE)
 	{
-		$value = ($line === '' OR ! isset($this->language[$line])) ? FALSE : $this->language[$line];
-
-		// Backward compatibility prior to adding $args parameter
-		if (is_bool($args))
-		{
-			$log_errors = $args;
-		}
+		$value = isset($this->language[$line]) ? $this->language[$line] : FALSE;
 
 		// Because killer robots like unicorns!
 		if ($value === FALSE && $log_errors === TRUE)
@@ -183,17 +177,9 @@ class CI_Lang {
 			log_message('error', 'Could not find the language line "'.$line.'"');
 		}
 
-		if ( ! is_bool($args) && ! empty($args))
-		{
-			if ( ! is_array($args))
-			{
-				$args = array($args);
-			}
-
-			return vsprintf($value, $args);
-		}
-
-		return $value;
+		return empty($args)
+						? $value
+						: vsprintf($value, $args);
 	}
 }
 
