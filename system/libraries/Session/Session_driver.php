028724aaa94f02c097c4a9e788e6f651379f5508
Kakysha <ezhikvdele@gmail.com>
2014-09-17 03:57:42 +0400

    return back cookie config into session driver

--- system/libraries/Session/Session_driver.php
+++ system/libraries/Session/Session_driver.php
@@ -121,6 +121,11 @@ abstract class CI_Session_driver implements SessionHandlerInterface {
 
 			property_exists($this, $key) && $this->$key = $value;
 		}
+		
+		isset($this->_cookie_domain) OR $this->_cookie_domain = config_item('cookie_domain');
+		isset($this->_cookie_path) OR $this->_cookie_path = config_item('cookie_path');
+		isset($this->_cookie_secure) OR $this->_cookie_secure = config_item('cookie_secure');
+		isset($this->_cookie_httponly) OR $this->_cookie_httponly = config_item('cookie_httponly');
 
 		// Pass our configuration to php.ini, when appropriate
 		ini_set('session.name', $this->_cookie_name);
