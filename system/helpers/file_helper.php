f9a615a5a304a2ead573d6e2869ee4ec7620511e
Andrey Andreev <narf@devilix.net>
2014-09-28 20:24:06 +0300

    [ci skip] Remove references to 'PHP5' from comments

--- system/helpers/file_helper.php
+++ system/helpers/file_helper.php
@@ -225,7 +225,7 @@ if ( ! function_exists('get_dir_file_info'))
 				$source_dir = rtrim(realpath($source_dir), DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR;
 			}
 
-			// foreach (scandir($source_dir, 1) as $file) // In addition to being PHP5+, scandir() is simply not as fast
+			// Used to be foreach (scandir($source_dir, 1) as $file), but scandir() is simply not as fast
 			while (FALSE !== ($file = readdir($fp)))
 			{
 				if (is_dir($source_dir.$file) && $file[0] !== '.' && $top_level_only === FALSE)
