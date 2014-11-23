538d53464eef25ae24205edb400a683a51602a2d
Jacques du Rand <jacquesdr@gmail.com>
2014-11-23 11:35:22 +0200

    Added missing comma at EOL(right place)

--- application/config/user_agents.php
+++ application/config/user_agents.php
@@ -237,12 +237,12 @@ $robots = array(
 	'fastcrawler'	=> 'FastCrawler',
 	'infoseek'		=> 'InfoSeek Robot 1.0',
 	'lycos'			=> 'Lycos',
-	'yandex'		=> 'YandexBot'
+	'yandex'		=> 'YandexBot',
 	'mediapartners-google'	=> 'MediaPartners Google',
 	'CRAZYWEBCRAWLER'	=> 'Crazy Webcrawler',
 	'adsbot-google'		=> 'AdsBot Google',
 	'feedfetcher-google'	=> 'Feedfetcher Google',
-	'curious george'	=> 'Curious George',
+	'curious george'	=> 'Curious George'
 );
 
 /* End of file user_agents.php */
