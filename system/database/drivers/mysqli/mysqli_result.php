221cdc0aaddb2b5642716f504bab6394b39adf49
Zachary <zknight@fsw.edu>
2014-12-08 11:53:40 -0500

    Revert "Added next_result() function to mysqli driver"
    This reverts commit 66dd66b14b94de7bfde13fb97dfeac1da2d2eae4.

--- system/database/drivers/mysqli/mysqli_result.php
+++ system/database/drivers/mysqli/mysqli_result.php
@@ -140,20 +140,6 @@ class CI_DB_mysqli_result extends CI_DB_result {
 	// --------------------------------------------------------------------
 
 	/**
-	* Read the next result
-	*
-	* @return  null
-	*/   
-	function next_result()
-	{
-		if (is_object($this->conn_id))
-		{
-			return mysqli_next_result($this->conn_id);
-		}
-	}
-	// -------------------------------------------------------------------- 
-
-	/**
 	 * Data Seek
 	 *
 	 * Moves the internal pointer to the desired offset. We call
