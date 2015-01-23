404f94036c85130f7d2b4ecba725edae927bbe8d
vlakoff <vlakoff@gmail.com>
2015-01-23 23:00:32 +0100

    Better error reporting if problematic session path

--- system/libraries/Session/drivers/Session_files_driver.php
+++ system/libraries/Session/drivers/Session_files_driver.php
@@ -112,9 +112,9 @@ class CI_Session_files_driver extends CI_Session_driver implements SessionHandle
 	 */
 	public function open($save_path, $name)
 	{
-		if ( ! is_dir($save_path) && ! mkdir($save_path, 0700, TRUE))
+		if ( ! is_dir($save_path) && ! @mkdir($save_path, 0700, TRUE))
 		{
-			log_message('error', "Session: Configured save path '".$this->_config['save_path']."' is not a directory, doesn't exist or cannot be created.");
+			trigger_error("Session: Configured save path '".$this->_config['save_path']."' is not a directory, doesn't exist or cannot be created.", E_USER_WARNING);
 			return FALSE;
 		}
 
