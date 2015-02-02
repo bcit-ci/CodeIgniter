1bd697cd7fcfd99ff292f26e697bb192901e1a41
Andrey Andreev <narf@devilix.net>
2015-02-02 14:07:57 +0200

    [ci skip] Improve note about sess_save_path in config.php

--- application/config/config.php
+++ application/config/config.php
@@ -328,11 +328,13 @@ $config['encryption_key'] = '';
 |
 | 'sess_save_path'
 |
-|	The location to save sessions to, driver dependant
+|	The location to save sessions to, driver dependant.
 |
-|	For the 'files' driver, it's a path to a directory.
+|	For the 'files' driver, it's a path to a writable directory.
 |	For the 'database' driver, it's a table name.
 |	Please read up the manual for the format with other session drivers.
+|
+|	IMPORTANT: You are REQUIRED to set a valid save path!
 |
 | 'sess_match_ip'
 |
