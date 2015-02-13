aa3a8432ee42b759b756595f1f347a168d741bef
milroy <milroy@outlook.com>
2015-02-13 17:59:14 +0530

    integrate gulp and elixir

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var elixir = require('laravel-elixir');
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Codeigniter application. By default, we are compiling the Less
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+
+elixir(function(mix) {
+
+    mix.less('app.less');
+
+});
