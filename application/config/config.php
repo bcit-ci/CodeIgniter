c5f74f196c5288adb08f965101faffbf7da8e722
Matthew Bootherstone <info@riwakawebsitedesigns.com>
2015-02-16 22:33:48 +1000

    Added Missing Permitted Uri chars

--- application/config/config.php
+++ application/config/config.php
@@ -154,7 +154,7 @@ $config['composer_autoload'] = FALSE;
 | DO NOT CHANGE THIS UNLESS YOU FULLY UNDERSTAND THE REPERCUSSIONS!!
 |
 */
-$config['permitted_uri_chars'] = 'a-z 0-9~%.:_\-';
+$config['permitted_uri_chars'] = 'a-z 0-9~%\.\:_\-&=$';
 
 
 /*
