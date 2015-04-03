67881e1c77ba0fc976d611a9e64bb17abc1df3a3
Caraka Media <joenist99@gmail.com>
2015-04-04 03:10:40 +0700

    Add the name of the directory in the array key.
    When I have two files are the same in the sub directory the file is in the root directory will not be listed. probably due to an array of key using the file name, so it will override the existing file in the subdirectory.
    
    Example:
    /dir_a/
    |- file_a.jpg
    |- /sub_dir_a/
       |- file_a.jpg
    
    Will generate :
    [file_a.jpg] => Array
            (
                [name] => file_a.jpg
                [server_path] => /dir_a/sub_dir_a/file_a.jpg
                [size] => 103675
                [date] => 1425054886
                [relative_path] => /dir_a/sub_dir_a
            )
    
    After adding the name of the directory in the array key:
    [dir_a/file_a.jpg] => Array
            (
                [name] => file_a.jpg
                [server_path] => /dir_a/file_a.jpg
                [size] => 103675
                [date] => 1425054886
                [relative_path] => /dir_a
            )
    [sub_dir_a/file_a.jpg] => Array
            (
                [name] => file_a.jpg
                [server_path] => /dir_a/sub_dir_a/file_a.jpg
                [size] => 103675
                [date] => 1425054886
                [relative_path] => /dir_a/sub_dir_a
            )

--- system/helpers/file_helper.php
+++ system/helpers/file_helper.php
@@ -245,8 +245,9 @@ if ( ! function_exists('get_dir_file_info'))
 				}
 				elseif ($file[0] !== '.')
 				{
-					$_filedata[$file] = get_file_info($source_dir.$file);
-					$_filedata[$file]['relative_path'] = $relative_path;
+					$_filekey = basename($source_dir) . "/" . $file;
+					$_filedata[$_filekey] = get_file_info($source_dir.$file);
+					$_filedata[$_filekey]['relative_path'] = $relative_path;
 				}
 			}
 
