182edc21bc6b0c0269660ef50a358b443e4ebad0
DevotionGeo <arifhussain.33@gmail.com>
2015-04-28 18:12:11 +0500

    Update form_validation_lang.php

--- system/language/english/form_validation_lang.php
+++ system/language/english/form_validation_lang.php
@@ -29,3 +29,4 @@ $lang['greater_than']			= "The %s field must contain a number greater than %s.";
  
 /* End of file form_validation_lang.php */
 /* Location: ./system/language/english/form_validation_lang.php */
+ 
