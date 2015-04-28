b7e0b29e948eda23eb29c46780cd2cef946b2291
DevotionGeo <arifhussain.33@gmail.com>
2015-04-28 13:21:33 +0500

    Update Form_validation.php
    alpha_space is great for form validation in case someone have an input field like Full Name, where a user can enter his/her full name comprising two or more names, like "Arif Bangash" or "Arif Hussain Bangash"
    
    Please also see the edit in system > language > english > form_validation_lang.php
    I am not very experienced in using github, sorry if I committed the change in a wrong way, i don't know if it should be directly committed to the 2.2-stable branch or to a new branch.

--- system/libraries/Form_validation.php
+++ system/libraries/Form_validation.php
@@ -1105,6 +1105,20 @@ class CI_Form_validation {
 	// --------------------------------------------------------------------
 
 	/**
+	 * Alpha-space
+	 *
+	 * @access	public
+	 * @param	string
+	 * @return	bool
+	 */
+	public function alpha_space($str)
+	{
+		return (! preg_match("/^([a-z ])+$/i", $str)) ? FALSE : TRUE;
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
 	 * Alpha-numeric
 	 *
 	 * @access	public
