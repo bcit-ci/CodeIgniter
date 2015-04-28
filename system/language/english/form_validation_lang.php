65da4ac97d734e74b0a8d09a27402fa93948075f
DevotionGeo <arifhussain.33@gmail.com>
2015-04-28 18:11:48 +0500

    Update form_validation_lang.php

--- system/language/english/form_validation_lang.php
+++ system/language/english/form_validation_lang.php
@@ -26,6 +26,6 @@ $lang['decimal']				= "The %s field must contain a decimal number.";
 $lang['less_than']				= "The %s field must contain a number less than %s.";
 $lang['greater_than']			= "The %s field must contain a number greater than %s.";
 
-
+ 
 /* End of file form_validation_lang.php */
 /* Location: ./system/language/english/form_validation_lang.php */
