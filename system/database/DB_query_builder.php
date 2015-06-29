bcit-ci/CodeIgniter

commit 59d1ffd927ef3b63dbadf42e1caf14957201fd2d

Fix #3932

system/database/DB_query_builder.php
@@ -2326,7 +2326,7 @@ abstract class CI_DB_query_builder extends CI_DB_driver {
 
 				// Split multiple conditions
 				$conditions = preg_split(
-					'/(\s*AND\s+|\s*OR\s+)/i',
+					'/(\s+AND\s+|\s+OR\s+)/i',
 					$this->{$qb_key}[$i]['condition'],
 					-1,
 					PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY

commit 7ed93be5325e1622c33ea60bf5f8fc7c42ad8938

Fix regression caused by 59d1ffd927ef3b63dbadf42e1caf14957201fd2d

system/database/DB_query_builder.php
@@ -2326,7 +2326,7 @@ abstract class CI_DB_query_builder extends CI_DB_driver {
 
 				// Split multiple conditions
 				$conditions = preg_split(
-					'/(\s+AND\s+|\s+OR\s+)/i',
+					'/((^|\s+)AND\s+|(^|\s+)OR\s+)/i',
 					$this->{$qb_key}[$i]['condition'],
 					-1,
 					PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY