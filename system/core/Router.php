b23ef09c5d13e4a04976f2e56ef4d1666a40f99e
Stefano Mozart <stefanomozart@sporos.com.br>
2015-07-07 12:17:37 -0300

    Update Router.php
    Correcting the problem in the _set_default_router, that forced the defult controller to be in the controllers directory

--- system/core/Router.php
+++ system/core/Router.php
@@ -295,26 +295,9 @@ class CI_Router {
 			show_error('Unable to determine what should be displayed. A default route has not been specified in the routing file.');
 		}
 
-		// Is the method being specified?
-		if (sscanf($this->default_controller, '%[^/]/%s', $class, $method) !== 2)
-		{
-			$method = 'index';
-		}
-
-		if ( ! file_exists(APPPATH.'controllers/'.$this->directory.ucfirst($class).'.php'))
-		{
-			// This will trigger 404 later
-			return;
-		}
-
-		$this->set_class($class);
-		$this->set_method($method);
-
-		// Assign routed segments, index starting from 1
-		$this->uri->rsegments = array(
-			1 => $class,
-			2 => $method
-		);
+		// If a default_controller rout is present, use the _set_request method to
+		// process the route
+		$this->_set_request(explode("/", $this->default_controller));
 
 		log_message('debug', 'No URI present. Default controller set.');
 	}
