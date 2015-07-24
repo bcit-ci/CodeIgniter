1497d4ec5adb8af92d54e1f321cac792bf0bc15e
kenjis <kenji.uui@gmail.com>
2015-07-25 08:57:00 +0900

    Fix alignment
    Signed-off-by: Kenji Suzuki <kenji.uui@gmail.com>

--- application/views/errors/cli/error_php.php
+++ application/views/errors/cli/error_php.php
@@ -2,9 +2,9 @@
 
 A PHP Error was encountered
 
-Severity: <?php echo $severity, "\n"; ?>
-Message:  <?php echo $message, "\n"; ?>
-Filename: <?php echo $filepath, "\n"; ?>
+Severity:    <?php echo $severity, "\n"; ?>
+Message:     <?php echo $message, "\n"; ?>
+Filename:    <?php echo $filepath, "\n"; ?>
 Line Number: <?php echo $line; ?>
 
 <?php if (defined('SHOW_DEBUG_BACKTRACE') && SHOW_DEBUG_BACKTRACE === TRUE): ?>
