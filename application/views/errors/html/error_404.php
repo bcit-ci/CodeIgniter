08901281fdc5e43680b5f0f7185ede42f0929108
Diederik Lascaris <DiederikLascaris@users.noreply.github.com>
2015-08-07 16:53:28 +0200

    Update error_404.php

--- application/views/errors/html/error_404.php
+++ application/views/errors/html/error_404.php
@@ -2,63 +2,115 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="utf-8">
-<title>404 Page Not Found</title>
-<style type="text/css">
 
-::selection { background-color: #E13300; color: white; }
-::-moz-selection { background-color: #E13300; color: white; }
+	<meta charset="utf-8">
 
-body {
-	background-color: #fff;
-	margin: 40px;
-	font: 13px/20px normal Helvetica, Arial, sans-serif;
-	color: #4F5155;
-}
+	<title>404 Page Not Found</title>
 
-a {
-	color: #003399;
-	background-color: transparent;
-	font-weight: normal;
-}
+	<style type="text/css">
 
-h1 {
-	color: #444;
-	background-color: transparent;
-	border-bottom: 1px solid #D0D0D0;
-	font-size: 19px;
-	font-weight: normal;
-	margin: 0 0 14px 0;
-	padding: 14px 15px 10px 15px;
-}
+        ::selection {
+        	background-color: #F07746; color: white;
+        }
 
-code {
-	font-family: Consolas, Monaco, Courier New, Courier, monospace;
-	font-size: 12px;
-	background-color: #f9f9f9;
-	border: 1px solid #D0D0D0;
-	color: #002166;
-	display: block;
-	margin: 14px 0 14px 0;
-	padding: 12px 10px 12px 10px;
-}
+        ::-moz-selection {
+        	background-color: #F07746; color: white;
+        }
 
-#container {
-	margin: 10px;
-	border: 1px solid #D0D0D0;
-	box-shadow: 0 0 8px #D0D0D0;
-}
+        body {
+            background-color: #FFF;
+            margin: 40px;
+            font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
+            font-size: 16px;
+            line-height: 1.5;
+            font-weight: normal;
+            color: #808080;
+        }
 
-p {
-	margin: 12px 15px 12px 15px;
-}
-</style>
+        a {
+            color: #dd4814;
+            background-color: transparent;
+            font-weight: normal;
+            text-decoration: none;
+        }
+
+        a:hover {
+            color: #97310e;
+        }
+
+        h1 {
+            color: #FFF;
+            background-color: #DD4814;
+            border-bottom: 1px solid #D0D0D0;
+            font-weight: bold;
+            margin: 0 0 14px 0;
+            padding: 5px 15px;
+            line-height: 40px;
+            font-size: 22px;
+        }
+
+        h2 {
+            color:#404040;
+            margin:0;
+            padding:0 0 10px 0;
+        }
+
+        code {
+            font-family: Consolas, Monaco, Courier New, Courier, monospace;
+            font-size: 13px;
+            background-color: #F5F5F5;
+            border: 1px solid #E3E3E3;
+            border-radius: 4px;
+            color: #002166;
+            display: block;
+            margin: 14px 0 14px 0;
+            padding: 12px 10px 12px 10px;
+        }
+
+        #body {
+            margin: 0 15px 0 15px;
+            min-height: 96px;
+        }
+
+        p {
+             margin: 0 0 10px;
+             padding:0;
+        }
+
+        #container {
+            margin: 10px;
+            border: 1px solid #D0D0D0;
+            box-shadow: 0 0 8px #D0D0D0;
+            border-radius: 4px;
+        }
+
+        #wrapper {
+            margin:0 auto;
+            max-width: 1024px;
+        }
+
+    </style>
+
 </head>
+
 <body>
-	<div id="container">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
+
+	<div id="wrapper">
+
+		<div id="container">
+
+			<h1><?php echo $heading; ?></h1>
+
+			<div id="body">
+				<?php echo $message; ?>
+			</div>
+
+		</div>
+
 	</div>
+
 </body>
+
 </html>
