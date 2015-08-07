2ead61d153e7f56a685c399e7d54abfd744585f3
Diederik Lascaris <DiederikLascaris@users.noreply.github.com>
2015-08-07 16:55:57 +0200

    Update error_php.php

--- application/views/errors/html/error_php.php
+++ application/views/errors/html/error_php.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
-<div style="border:1px solid #990000;padding-left:20px;margin:0 0 10px 0;">
+<div style="border:1px solid #DD4814;padding-left:20px;margin:10px 0;">
 
 <h4>A PHP Error was encountered</h4>
 
