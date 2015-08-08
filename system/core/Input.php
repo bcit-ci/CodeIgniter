a88290e217b7f94d3b2234713d0149cc9cdf4ac6
Thomas Rieschl <thomas@trinet.at>
2015-08-08 11:09:43 +0200

    fix E_NOTICE error "Undefined index: REMOTE_ADDR" when requesting `$this->input->ip_address()` on CLI

--- system/core/Input.php
+++ system/core/Input.php
@@ -349,7 +349,7 @@ class CI_Input {
 		}
 		else
 		{
-			$this->ip_address = $_SERVER['REMOTE_ADDR'];
+			$this->ip_address = $this->server('REMOTE_ADDR');
 		}
 
 		if ( ! $this->valid_ip($this->ip_address))
