8bff083b7e03f1aedeb006a7e4abb59f85f6d362
Hedcler Morais <hedclermorais@gmail.com>
2015-08-12 16:03:25 -0300

    Update CodeIgniter.php
    Fixes bug when the configuration is separated into directories.

--- system/core/CodeIgniter.php
+++ system/core/CodeIgniter.php
@@ -65,9 +65,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	if (file_exists(APPPATH.'config/'.ENVIRONMENT.'/constants.php'))
 	{
 		require_once(APPPATH.'config/'.ENVIRONMENT.'/constants.php');
+	} else {
+		require_once(APPPATH.'config/constants.php');
 	}
 
-	require_once(APPPATH.'config/constants.php');
 
 /*
  * ------------------------------------------------------
