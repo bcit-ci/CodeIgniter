3cfdbb4dcb784f3584ceff67ecf501180ce85bce
Exclumice <evansiveyt@gmail.com>
2015-08-18 10:18:55 +0300

    returned httponly flag

--- application/config/config.php
+++ application/config/config.php
@@ -271,6 +271,7 @@ $config['cookie_prefix']	= "";
 $config['cookie_domain']	= "";
 $config['cookie_path']		= "/";
 $config['cookie_secure']	= FALSE;
+$config['cookie_httponly']  = FALSE;
 
 /*
 |--------------------------------------------------------------------------
