6d6b3b2f89d517b4589cb52965f34b115036584a
Ahmad Anbar <aanbar@gmail.com>
2015-10-10 22:51:54 +0300

    Optimize csv_from_result speed.

--- system/database/DB_utility.php
+++ system/database/DB_utility.php
@@ -254,11 +254,12 @@ abstract class CI_DB_utility {
 		// Next blast through the result array and build out the rows
 		while ($row = $query->unbuffered_row('array'))
 		{
+			$line = array();
 			foreach ($row as $item)
 			{
-				$out .= $enclosure.str_replace($enclosure, $enclosure.$enclosure, $item).$enclosure.$delim;
+				$line[] = $enclosure.str_replace($enclosure, $enclosure.$enclosure, $item).$enclosure;
 			}
-			$out = substr($out, 0, -strlen($delim)).$newline;
+			$out .= implode($delim, $line).$newline;
 		}
 
 		return $out;
