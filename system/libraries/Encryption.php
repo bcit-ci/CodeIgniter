5afa348b48a93f24957377dc12f86ae64665b944
Andrey Andreev <narf@devilix.net>
2015-11-24 11:48:39 +0200

    Use PHP7's random_bytes() when possible
    Close #4260

--- system/libraries/Encryption.php
+++ system/libraries/Encryption.php
@@ -337,6 +337,11 @@ class CI_Encryption {
 	 */
 	public function create_key($length)
 	{
+		if (function_exists('random_bytes'))
+		{
+			return random_bytes((int) $length);
+		}
+
 		return ($this->_driver === 'mcrypt')
 			? mcrypt_create_iv($length, MCRYPT_DEV_URANDOM)
 			: openssl_random_pseudo_bytes($length);
