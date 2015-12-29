514b22b65e9a07a894091afd1fc1dbc2ea17b902
Shanavas M <shanavas.m2@gmail.com>
2015-12-29 23:16:07 +0530

    Check if input value already exists in the specified database field
    Signed-off-by: Shanavas M <shanavas.m2@gmail.com>

--- system/libraries/Form_validation.php
+++ system/libraries/Form_validation.php
@@ -1132,6 +1132,26 @@ class CI_Form_validation {
 	// --------------------------------------------------------------------
 
 	/**
+	 * Exists
+	 *
+	 * Check if the input value already exists
+	 * in the specified database field
+	 *
+	 * @param	string	$str
+	 * @param	string	$field
+	 * @return	bool
+	 */
+	public function exists($str, $field)
+	{
+		sscanf($field, '%[^.].%[^.]', $table, $field);
+		return isset($this->CI->db)
+			? ($this->CI->db->limit(1)->get_where($table, array($field => $str))->num_rows() === 1)
+				: FALSE;
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
 	 * Minimum Length
 	 *
 	 * @param	string
