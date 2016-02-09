f3bb999c08bd4255b36a1b42a2f12c926f102e6e
ViVers <botkajozef@hotmail.com>
2016-02-09 12:16:22 +0100

    Fix Cascade View
    Fixed cascade view for loading packages by (mwhitneysdsu)

--- system/core/Loader.php
+++ system/core/Loader.php
@@ -887,18 +887,16 @@ class CI_Loader {
 			$_ci_ext = pathinfo($_ci_view, PATHINFO_EXTENSION);
 			$_ci_file = ($_ci_ext === '') ? $_ci_view.'.php' : $_ci_view;
 
-			foreach ($this->_ci_view_paths as $_ci_view_file => $cascade)
+			foreach ($this->_ci_view_paths as $view_file => $cascade)
 			{
-				if (file_exists($_ci_view_file.$_ci_file))
+				if (file_exists($view_file.$_ci_file))
 				{
-					$_ci_path = $_ci_view_file.$_ci_file;
+					$_ci_path = $view_file.$_ci_file;
 					$file_exists = TRUE;
-					break;
-				}
-
-				if ( ! $cascade)
-				{
-					break;
+					if ( ! $cascade)
+					{
+						break;
+					}
 				}
 			}
 		}
