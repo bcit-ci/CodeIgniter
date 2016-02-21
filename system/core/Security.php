07c03d09c4f7f5762babf1bc532f508b891a2101
medhavini <medhajpr@gmail.com>
2016-02-21 16:48:16 +0530

    strip_image_tags() bug fixed
    The earlier version of strip_image_tags() displays only the first letter of image URL when neither single quotes or double quotes are used to enclose the image URL.
    
    This is fixed by making the regex greedy again, and it can be non-greedy if URL is followed by at least 1 whitespace.

--- system/core/Security.php
+++ system/core/Security.php
@@ -762,7 +762,7 @@ class CI_Security {
 	 */
 	public function strip_image_tags($str)
 	{
-		return preg_replace(array('#<img[\s/]+.*?src\s*=\s*["\'](.+?)["\'].*?\>#', '#<img[\s/]+.*?src\s*=\s*(.+?).*?\>#'), '\\1', $str);
+		return preg_replace(array('#<img[\s/]+.*?src\s*=\s*["\'](.+?)["\'].*?\>#', '#<img[\s/]+.*?src\s*=\s*(.+?\s+|.+).*?\>#'), '\\1', $str);
 	}
 
 	// ----------------------------------------------------------------
