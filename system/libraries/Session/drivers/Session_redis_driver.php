7f0f73ba81ad712f2553e2f7ef5d0a50f16e119e
__RD <beantang.tang@gmail.com>
2016-02-29 22:56:29 +0800

    delete lock directly -> call _release_lock()

--- system/libraries/Session/drivers/Session_redis_driver.php
+++ system/libraries/Session/drivers/Session_redis_driver.php
@@ -277,8 +277,7 @@ class CI_Session_redis_driver extends CI_Session_driver implements SessionHandle
 			try {
 				if ($this->_redis->ping() === '+PONG')
 				{
-					isset($this->_lock_key) && $this->_redis->delete($this->_lock_key);
-					$this->_lock_key = NULL;
+					$this->_release_lock($this->_lock_key);
 					if ($this->_redis->close() === $this->_failure)
 					{
 						return $this->_failure;
