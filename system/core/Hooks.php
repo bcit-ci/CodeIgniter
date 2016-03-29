d6cab530e5896bad4792fa0de9f24dfbc9dfcb01
panda <903098362@qq.com>
2016-03-29 15:02:50 +0800

    注释完

--- system/core/Hooks.php
+++ system/core/Hooks.php
@@ -55,6 +55,7 @@ class CI_Hooks {
 	 *
 	 * @var	bool
 	 */
+	// 是否开启hooks
 	public $enabled = FALSE;
 
 	/**
@@ -62,6 +63,7 @@ class CI_Hooks {
 	 *
 	 * @var	array
 	 */
+	// hooks数组
 	public $hooks =	array();
 
 	/**
@@ -69,6 +71,7 @@ class CI_Hooks {
 	 *
 	 * @var array
 	 */
+	//hooks调用到的对象
 	protected $_objects = array();
 
 	/**
@@ -78,6 +81,7 @@ class CI_Hooks {
 	 *
 	 * @var	bool
 	 */
+	//hooks是否在进程中，反正死循环
 	protected $_in_progress = FALSE;
 
 	/**
@@ -87,16 +91,19 @@ class CI_Hooks {
 	 */
 	public function __construct()
 	{
+		//加载配置文件
 		$CFG =& load_class('Config', 'core');
 		log_message('info', 'Hooks Class Initialized');
 
 		// If hooks are not enabled in the config file
 		// there is nothing else to do
+		//如果配置文件为开启hooks，直接返回
 		if ($CFG->item('enable_hooks') === FALSE)
 		{
 			return;
 		}
 
+		//加载hooks的配置文件
 		// Grab the "hooks" definition file.
 		if (file_exists(APPPATH.'config/hooks.php'))
 		{
@@ -107,14 +114,17 @@ class CI_Hooks {
 		{
 			include(APPPATH.'config/'.ENVIRONMENT.'/hooks.php');
 		}
-
+		
+		//检查配置文件中定义的hook数组是否正确
 		// If there are no hooks, we're done.
 		if ( ! isset($hook) OR ! is_array($hook))
 		{
 			return;
 		}
-
+		
+		//将配置hook数组赋值给对象hooks属性
 		$this->hooks =& $hook;
+		//更新enabled，允许使用hooks
 		$this->enabled = TRUE;
 	}
 
@@ -132,20 +142,25 @@ class CI_Hooks {
 	 */
 	public function call_hook($which = '')
 	{
+		//判断是否开启hooks或者改hooks是否存在
 		if ( ! $this->enabled OR ! isset($this->hooks[$which]))
 		{
 			return FALSE;
 		}
-
+	
+		//如果配置是数组，则判断是否设置该hooks函数 $hook['pre_system'] = array('class'=>'','function'=>''...)
 		if (is_array($this->hooks[$which]) && ! isset($this->hooks[$which]['function']))
 		{
+			//遍历改hooks下面每个函数
 			foreach ($this->hooks[$which] as $val)
 			{
+				//调用
 				$this->_run_hook($val);
 			}
 		}
 		else
 		{
+			//如果是直接配置函数 $hook['pre_system'] = hook_function
 			$this->_run_hook($this->hooks[$which]);
 		}
 
@@ -165,15 +180,17 @@ class CI_Hooks {
 	protected function _run_hook($data)
 	{
 		// Closures/lambda functions and array($object, 'method') callables
+		//是否可调用,包括lamdda 函数表达式和 array($object,'method')
 		if (is_callable($data))
 		{
+			//如果是是数组，$object->method，否则，直接通过函数名调用函数
 			is_array($data)
 				? $data[0]->{$data[1]}()
 				: $data();
 
 			return TRUE;
 		}
-		elseif ( ! is_array($data))
+		elseif ( ! is_array($data))	//数据不是数组，返回
 		{
 			return FALSE;
 		}
@@ -181,6 +198,7 @@ class CI_Hooks {
 		// -----------------------------------
 		// Safety - Prevents run-away loops
 		// -----------------------------------
+		//防止死循环
 
 		// If the script being called happens to have the same
 		// hook call within it a loop can happen
@@ -192,40 +210,48 @@ class CI_Hooks {
 		// -----------------------------------
 		// Set file path
 		// -----------------------------------
-
+		//array（'class','method','filename','filepath'）这种形式hook配置参数检查
 		if ( ! isset($data['filepath'], $data['filename']))
 		{
 			return FALSE;
 		}
-
+		
+		//拼接文件路径
 		$filepath = APPPATH.$data['filepath'].'/'.$data['filename'];
-
+		
+		//判断文件是否存在
 		if ( ! file_exists($filepath))
 		{
 			return FALSE;
 		}
 
 		// Determine and class and/or function names
-		$class		= empty($data['class']) ? FALSE : $data['class'];
-		$function	= empty($data['function']) ? FALSE : $data['function'];
-		$params		= isset($data['params']) ? $data['params'] : '';
-
+		$class		= empty($data['class']) ? FALSE : $data['class'];	//类
+		$function	= empty($data['function']) ? FALSE : $data['function'];	//方法
+		$params		= isset($data['params']) ? $data['params'] : '';	//参数
+		
+		//方法是否为空
 		if (empty($function))
 		{
 			return FALSE;
 		}
 
 		// Set the _in_progress flag
+		//设置 进程中 。。。
 		$this->_in_progress = TRUE;
 
 		// Call the requested class and/or function
+		//类调用
 		if ($class !== FALSE)
 		{
 			// The object is stored?
+			//该类是否已经初始化
 			if (isset($this->_objects[$class]))
 			{
+				//是否存在该方法
 				if (method_exists($this->_objects[$class], $function))
 				{
+					//调用方法
 					$this->_objects[$class]->$function($params);
 				}
 				else
@@ -235,30 +261,33 @@ class CI_Hooks {
 			}
 			else
 			{
+				//加载该类
 				class_exists($class, FALSE) OR require_once($filepath);
-
+				//检查类和方法是否有效
 				if ( ! class_exists($class, FALSE) OR ! method_exists($class, $function))
 				{
 					return $this->_in_progress = FALSE;
 				}
 
 				// Store the object and execute the method
+				//缓存类和调用方法
 				$this->_objects[$class] = new $class();
 				$this->_objects[$class]->$function($params);
 			}
 		}
-		else
+		else //方法调用
 		{
+			//载入函数文件
 			function_exists($function) OR require_once($filepath);
-
+			//检查函数是否存在
 			if ( ! function_exists($function))
 			{
 				return $this->_in_progress = FALSE;
 			}
-
+			//调用函数
 			$function($params);
 		}
-
+		//结束
 		$this->_in_progress = FALSE;
 		return TRUE;
 	}
