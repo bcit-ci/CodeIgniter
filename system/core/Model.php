23ac4c6510221b3cbf3a23fde7c3942f585bf357
Plinfut <plinfut@gmail.com>
2016-07-22 19:52:22 +0200

    Fix: Code completion
    Added properties to Controller, Model and Migration to enable code
    completion in popular IDEs.

--- system/core/Model.php
+++ system/core/Model.php
@@ -45,6 +45,36 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @category	Libraries
  * @author		EllisLab Dev Team
  * @link		https://codeigniter.com/user_guide/libraries/config.html
+ * @property CI_Benchmark $benchmark
+ * @property CI_Cache $cache
+ * @property CI_Calendar $calendar
+ * @property CI_Config $config
+ * @property CI_Email $email
+ * @property CI_Encryption $encryption
+ * @property CI_Upload $upload
+ * @property CI_Form_validation $form_validation
+ * @property CI_FTP $ftp
+ * @property CI_Image_lib $image_lib
+ * @property CI_Input $input
+ * @property CI_Lang $lang
+ * @property CI_Loader $load
+ * @property CI_Migration $migration
+ * @property CI_Output $output
+ * @property CI_Pagination $pagination
+ * @property CI_Parser $parser
+ * @property CI_Security $security
+ * @property CI_Session $session
+ * @property CI_Table $table
+ * @property CI_Trackback $trackback
+ * @property CI_Typography $typography
+ * @property CI_Unit_test $unit
+ * @property CI_URI $uri
+ * @property CI_User_agent $agent
+ * @property CI_Xmlrpc $xmlrpc
+ * @property CI_Xmlrpcs $xmlrpcs
+ * @property CI_Zip $zip
+ * @property CI_DB_query_builder $db
+ * @property CI_DB_forge $dbforge
  */
 class CI_Model {
 
