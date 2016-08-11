ae31a918a3c0eff331f3ae2f03afca25158e93ff
tianhe1986 <w1s2j3229@163.com>
2016-08-11 20:13:06 +0800

    Using "! isset($a, $b, ……)" instead of "! isset($a) OR ! isset($b) OR ……"
    Signed-off-by: tianhe1986 <w1s2j3229@163.com>

--- system/libraries/Cache/drivers/Cache_file.php
+++ system/libraries/Cache/drivers/Cache_file.php
@@ -227,7 +227,7 @@ class CI_Cache_file extends CI_Driver {
 		{
 			$mtime = filemtime($this->_cache_path.$id);
 
-			if ( ! isset($data['ttl']) OR ! isset($data['time']))
+			if ( ! isset($data['ttl'], $data['time']))
 			{
 				return FALSE;
 			}
