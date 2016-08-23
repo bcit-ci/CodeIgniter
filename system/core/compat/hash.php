24ad2dd11ed66bafe3a8a9224b310d7e4b005446
tianhe1986 <w1s2j3229@163.com>
2016-08-23 17:44:21 +0800

    Hash: processing algorithm name case-insensitively in hash_pbkdf2():
    Signed-off-by: tianhe1986 <w1s2j3229@163.com>

--- system/core/compat/hash.php
+++ system/core/compat/hash.php
@@ -119,6 +119,7 @@ if ( ! function_exists('hash_pbkdf2'))
 	 */
 	function hash_pbkdf2($algo, $password, $salt, $iterations, $length = 0, $raw_output = FALSE)
 	{
+		$algo = strtolower($algo);
 		if ( ! in_array($algo, hash_algos(), TRUE))
 		{
 			trigger_error('hash_pbkdf2(): Unknown hashing algorithm: '.$algo, E_USER_WARNING);
