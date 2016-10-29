d6df23953de816b78494809770e1117d4165c55e
Shoaib Zafar <contact@shoaibzafar.com>
2016-10-29 15:49:04 +0500

    Check filefino extention is loaded while uploading
    Added function to check either fileinfo extention is loaded or not.
    Before this check, if extention was not loaded, script jsut stop and
    there is no log of what happend. Now this will generate an error log and
    return.

--- system/language/english/upload_lang.php
+++ system/language/english/upload_lang.php
@@ -53,3 +53,4 @@ $lang['upload_no_filepath'] = 'The upload path does not appear to be valid.';
 $lang['upload_no_file_types'] = 'You have not specified any allowed file types.';
 $lang['upload_bad_filename'] = 'The file name you submitted already exists on the server.';
 $lang['upload_not_writable'] = 'The upload destination folder does not appear to be writable.';
+$lang['upload_fileinfo_notfound'] = 'Unable to load fileinfo extension.';
