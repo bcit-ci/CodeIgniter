55195e50aad2fbb0737008b39a5d686ff2e98997
DiederikLascaris <info@runesa.nl>
2017-01-22 23:20:58 +0100

    Removed wrapper and changed mix cases usage of hex colorcodes

--- application/views/errors/html/error_db.php
+++ application/views/errors/html/error_db.php
@@ -3,90 +3,84 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="utf-8">
-<title>Database Error</title>
-<style type="text/css">
+	<meta charset="utf-8">
+	<title>Database Error</title>
+	<style type="text/css">
 
-::selection { background-color: #F07746; color: white; }
-::-moz-selection { background-color: #F07746; color: white; }
+	::selection { background-color: #f07746; color: #fff; }
+	::-moz-selection { background-color: #f07746; color: #fff; }
 
-body {
-	background-color: #fff;
-	margin: 40px;
-	font: 16px/24px normal "Helvetica Neue", Helvetica, Arial, sans-serif;
-	color: #808080;
-}
+	body {
+		background-color: #fff;
+		margin: 40px auto;
+		max-width: 1024px;
+		font: 16px/24px normal "Helvetica Neue", Helvetica, Arial, sans-serif;
+		color: #808080;
+	}
 
-a {
-	color: #dd4814;
-	background-color: transparent;
-	font-weight: normal;
-	text-decoration: none;
-}
+	a {
+		color: #dd4814;
+		background-color: transparent;
+		font-weight: normal;
+		text-decoration: none;
+	}
 
-a:hover {
-	color: #97310e;
-}
+	a:hover {
+		color: #97310e;
+	}
 
-h1 {
-	color: #FFF;
-	background-color: #DD4814;
-	border-bottom: 1px solid #D0D0D0;
-	font-size: 22px;
-	font-weight: bold;
-	margin: 0 0 14px 0;
-	padding: 5px 15px;
-	line-height: 40px;
-}
+	h1 {
+		color: #fff;
+		background-color: #dd4814;
+		border-bottom: 1px solid #d0d0d0;
+		font-size: 22px;
+		font-weight: bold;
+		margin: 0 0 14px 0;
+		padding: 5px 15px;
+		line-height: 40px;
+	}
 
-h2 {
-	color:#404040;
-	margin:0;
-	padding:0 0 10px 0;
-}
+	h2 {
+		color:#404040;
+		margin:0;
+		padding:0 0 10px 0;
+	}
 
-code {
-	font-family: Consolas, Monaco, Courier New, Courier, monospace;
-	font-size: 13px;
-	background-color: #F5F5F5;
-	border: 1px solid #E3E3E3;
-	border-radius: 4px;
-	color: #002166;
-	display: block;
-	margin: 14px 0 14px 0;
-	padding: 12px 10px 12px 10px;
-}
+	code {
+		font-family: Consolas, Monaco, Courier New, Courier, monospace;
+		font-size: 13px;
+		background-color: #f5f5f5;
+		border: 1px solid #e3e3e3;
+		border-radius: 4px;
+		color: #002166;
+		display: block;
+		margin: 14px 0 14px 0;
+		padding: 12px 10px 12px 10px;
+	}
 
-#container {
-	margin: 10px;
-	border: 1px solid #D0D0D0;
-	box-shadow: 0 0 8px #D0D0D0;
-	border-radius: 4px;
-}
+	#container {
+		margin: 10px;
+		border: 1px solid #d0d0d0;
+		box-shadow: 0 0 8px #d0d0d0;
+		border-radius: 4px;
+	}
 
-p {
-	margin: 0 0 10px;
-	padding:0;
-}
+	p {
+		margin: 0 0 10px;
+		padding:0;
+	}
 
-#wrapper {
-	margin:0 auto;
-	max-width: 1024px;
-}
-
-#body {
-	margin: 0 15px 0 15px;
-	min-height: 96px;
-}
-</style>
+	#body {
+		margin: 0 15px 0 15px;
+		min-height: 96px;
+	}
+	</style>
 </head>
 <body>
-	<div id="wrapper">
-		<div id="container">
-			<h1><?php echo $heading; ?></h1>
-			<div id="body">
-				<?php echo $message; ?>
-			</div>
+	<div id="container">
+		<h1><?php echo $heading; ?></h1>
+		<div id="body">
+			<?php echo $message; ?>
 		</div>
 	</div>
 </body>
