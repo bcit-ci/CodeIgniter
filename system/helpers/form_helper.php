5e4ab7df5d0dfa8cc4b8b048604df100382cbacc
lloricode <emorickfighter@gmail.com>
2017-01-24 19:18:24 +0800

    add attribute in form_dropdown in form_helper.php

--- system/helpers/form_helper.php
+++ system/helpers/form_helper.php
@@ -419,16 +419,49 @@ if ( ! function_exists('form_dropdown'))
 					continue;
 				}
 
-				$form .= '<optgroup label="'.$key."\">\n";
+                                if(array_key_exists('data', $val))
+                                {
+                                        $attributes_ = array();
+                                        
+                                        foreach ($val as $k_=>$v_)
+                                        {
+                                            $k_ = (string) $k_;
+                                            
+                                            if($k_ === 'data')
+                                            {
+                                                continue;
+                                            }
+                                            
+                                            $value__ = '';
+                                            
+                                            if(!array_key_exists('value', $val))
+                                            {
+                                                $value__='value="'.html_escape($key).'"';
+                                            }
+                                            
+                                            $attributes_[$k_] = $v_;
+                                        }
+                                        
+                                        $attributes_ = _attributes_to_string($attributes_);
+                                        
+                                        $form .= '<option '.$value__.$attributes_
+                                            .(in_array($key, $selected) ? ' selected="selected"' : '').'>'
+                                            .(string) $val['data']."</option>\n";
+                                }
+                                else
+                                {
+                                
+                                        $form .= '<optgroup label="'.$key."\">\n";
 
-				foreach ($val as $optgroup_key => $optgroup_val)
-				{
-					$sel = in_array($optgroup_key, $selected) ? ' selected="selected"' : '';
-					$form .= '<option value="'.html_escape($optgroup_key).'"'.$sel.'>'
-						.(string) $optgroup_val."</option>\n";
-				}
+                                        foreach ($val as $optgroup_key => $optgroup_val)
+                                        {
+                                                $sel = in_array($optgroup_key, $selected) ? ' selected="selected"' : '';
+                                                $form .= '<option value="'.html_escape($optgroup_key).'"'.$sel.'>'
+                                                        .(string) $optgroup_val."</option>\n";
+                                        }
 
-				$form .= "</optgroup>\n";
+                                        $form .= "</optgroup>\n";
+                                }
 			}
 			else
 			{
