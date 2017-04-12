23d5e7ac6de7bf8f46e02ba17ae3f9e2b020d1ea
elmersaid <said-elmers@hotmail.com>
2017-04-12 03:54:49 +0100

    Update html_helper.php " add script tag"

--- system/helpers/html_helper.php
+++ system/helpers/html_helper.php
@@ -389,3 +389,45 @@ if ( ! function_exists('meta'))
 		return $str;
 	}
 }
+
+
+
+if ( ! function_exists('script_tag')) {
+    function script_tag($src = '', $language = 'javascript', $type = 'text/javascript', $index_page = FALSE)
+    {
+        $CI =& get_instance();
+        $script = '<scr'.'ipt';
+        if (is_array($src)) {
+            foreach ($src as $k=>$v) {
+                if ($k == 'src' AND strpos($v, '://') === FALSE) {
+                    if ($index_page === TRUE) {
+                        $script .= ' src="'.$CI->config->site_url($v).'"';
+                    }
+                    else {
+                        $script .= ' src="'.$CI->config->slash_item('base_url').$v.'"';
+                    }
+                }
+                else {
+                    $script .= "$k=\"$v\"";
+                }
+            }
+
+            $script .= "></scr"."ipt>\n";
+        }
+        else {
+            if ( strpos($src, '://') !== FALSE) {
+                $script .= ' src="'.$src.'" ';
+            }
+            elseif ($index_page === TRUE) {
+                $script .= ' src="'.$CI->config->site_url($src).'" ';
+            }
+            else {
+                $script .= ' src="'.$CI->config->slash_item('base_url').$src.'" ';
+            }
+
+            $script .= 'language="'.$language.'" type="'.$type.'"';
+            $script .= ' /></scr'.'ipt>'."\n";
+        }
+        return $script;
+    }
+}
