cc279bf574d0cd056ec1919775cdb987a470bfdc
Kevin Morssink <contact@kevinmorssink.nl>
2017-04-21 23:40:07 +0200

    Fix potential XSS injection in hidden HTML attributes in form_hidden

--- system/helpers/form_helper.php
+++ system/helpers/form_helper.php
@@ -202,7 +202,7 @@ if ( ! function_exists('form_hidden'))
 
 		if ( ! is_array($value))
 		{
-			$form .= '<input type="hidden" name="'.$name.'" value="'.html_escape($value)."\" />\n";
+			$form .= '<input type="hidden" name="'.html_escape($name).'" value="'.html_escape($value)."\" />\n";
 		}
 		else
 		{
