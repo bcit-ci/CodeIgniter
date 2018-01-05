47df0884591b8678b6873be58b1dc571363e7b28
Andrey Andreev <narf@devilix.net>
2018-01-05 17:29:31 +0200

    [ci skip] Merge pull request #5369 from tianhe1986/develop_cache_memcache
    Cache_memcached: setting initial value with increment/decrement when not exist

--- system/libraries/Cache/drivers/Cache_memcached.php
+++ system/libraries/Cache/drivers/Cache_memcached.php
@@ -198,7 +198,12 @@ class CI_Cache_memcached extends CI_Driver {
 	 */
 	public function increment($id, $offset = 1)
 	{
-		return $this->_memcached->increment($id, $offset);
+		if (($result = $this->_memcached->increment($id, $offset)) === FALSE)
+		{
+			return $this->_memcached->add($id, $offset) ? $offset : FALSE;
+		}
+
+		return $result;
 	}
 
 	// ------------------------------------------------------------------------
@@ -212,7 +217,12 @@ class CI_Cache_memcached extends CI_Driver {
 	 */
 	public function decrement($id, $offset = 1)
 	{
-		return $this->_memcached->decrement($id, $offset);
+		if (($result = $this->_memcached->decrement($id, $offset)) === FALSE)
+		{
+			return $this->_memcached->add($id, 0) ? 0 : FALSE;
+		}
+
+		return $result;
 	}
 
 	// ------------------------------------------------------------------------
