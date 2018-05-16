26440a4fa2206b369623df5c8690f7bcd735a8e1
gonghaibin <gonghaibin@sogou-inc.com>
2018-05-16 18:50:59 +0800

    'xss_clean-fixbug'

--- system/core/Common.php
+++ system/core/Common.php
@@ -720,6 +720,8 @@ if ( ! function_exists('remove_invisible_characters'))
 			$non_displayables[] = '/%0[0-8bcef]/i';	// url encoded 00-08, 11, 12, 14, 15
 			$non_displayables[] = '/%1[0-9a-f]/i';	// url encoded 16-31
 			$non_displayables[] = '/%7f/i';	// url encoded 127
+			$non_displayables[] = '/%ff/i';	
+
 		}
 
 		$non_displayables[] = '/[\x00-\x08\x0B\x0C\x0E-\x1F\x7F]+/S';	// 00-08, 11, 12, 14-31, 127
