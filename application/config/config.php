718b7c9dfb24ceb666eeeffaf63df5385ec28b71
Lohith J <lohithj94@gmail.com>
2019-02-08 13:46:40 +0530

    corrected grammar

--- application/config/config.php
+++ application/config/config.php
@@ -13,9 +13,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 | WARNING: You MUST set this value!
 |
-| If it is not set, then CodeIgniter will try guess the protocol and path
-| your installation, but due to security concerns the hostname will be set
-| to $_SERVER['SERVER_ADDR'] if available, or localhost otherwise.
+| If it is not set, then CodeIgniter will try to guess the protocol and 
+| path your installation, but due to security concerns the hostname will 
+| be set to $_SERVER['SERVER_ADDR'] if available, or localhost otherwise.
 | The auto-detection mechanism exists only for convenience during
 | development and MUST NOT be used in production!
 |
