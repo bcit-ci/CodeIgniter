06739bdc05f2fa5fc92db0675ec48f7374ae50cf
Michael Long <mchobbylong@gmail.com>
2019-03-16 00:17:06 +0800

    Remove unnecessary code

--- system/libraries/Session/drivers/Session_redis_driver.php
+++ system/libraries/Session/drivers/Session_redis_driver.php
@@ -389,7 +389,7 @@ class CI_Session_redis_driver extends CI_Session_driver implements SessionHandle
 				sleep(1);
 				continue;
 			}
-			elseif ($ttl === -1 && ! $this->_redis->setex($lock_key, 300, time()))
+			elseif ( ! $this->_redis->setex($lock_key, 300, time()))
 			{
 				log_message('error', 'Session: Error while trying to obtain lock for '.$this->_key_prefix.$session_id);
 				return FALSE;
