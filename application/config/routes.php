143f7db81482b4ea53bca6f9b1d050b3b05b540b
Tosin Amuda <tosinamuda@gmail.com>
2019-05-02 23:06:48 +0100

    Change Default Controller

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 $route['home/index'] = 'home';
