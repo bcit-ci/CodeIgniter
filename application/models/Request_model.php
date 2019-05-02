e381f86fa7e211c8f5f593d729e6ccc93e9982b9
Tosin Amuda <tosinamuda@gmail.com>
2019-05-02 21:47:54 +0100

    Request API & Model

---
+++ application/models/Request_model.php
@@ -0,0 +1,29 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Request_model extends CI_Model {
+
+	public function __construct() {
+			parent::__construct();
+			 $this->load->database();
+    	}
+
+        public function get_upriselive_lead(){
+ 
+        $response = array();
+        
+        // Select record
+        $this->db->select('*');
+        $q = $this->db->get(TABLE_REQUESTS);
+        $response = $q->result_array();
+    
+        return $response;
+        }
+
+  
+  public function create($data){
+ 
+  return $this->db->insert(TABLE_REQUESTS, $data);
+  }
+
+  
+}
