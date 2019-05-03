8d0c6333e6b65f071576fd599a916768ee73e9ab
Tosin Amuda <tosinamuda@gmail.com>
2019-05-03 09:41:05 +0100

    Get Base Url From .env

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = getenv('BASE_URL');
 
 /*
 |--------------------------------------------------------------------------
