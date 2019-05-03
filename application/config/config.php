8dca81b305e18194cab146da053ec62057b3bb20
Tosin Amuda <tosinamuda@gmail.com>
2019-05-03 09:54:56 +0100

    Test Getenv inside config

--- application/config/config.php
+++ application/config/config.php
@@ -24,6 +24,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] = getenv('BASE_URL');
+echo getenv('BASE_URL');
+exit;
 
 /*
 |--------------------------------------------------------------------------
