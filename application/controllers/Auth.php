095a6c2de395a309afb776472094c8240c1ccf4f
Tosin Amuda <tosinamuda@gmail.com>
2019-05-03 11:12:29 +0100

    Add authentication to dashboard

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -81,7 +81,7 @@ class Auth extends CI_Controller
 				//if the login is successful
 				//redirect them back to the home page
 				$this->session->set_flashdata('message', $this->ion_auth->messages());
-				redirect('/', 'refresh');
+				redirect('dashboard', 'refresh');
 			}
 			else
 			{
--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -9,12 +9,31 @@ class Dashboard extends CI_Controller {
 		$this->load->library('session');
 		$this->load->library('phpmailer');
 		$this->load->model('request_model');
+		$this->load->library(['ion_auth', 'form_validation']);
+		$this->load->helper(['url', 'language']);
+
+		$this->form_validation->set_error_delimiters($this->config->item('error_start_delimiter', 'ion_auth'), $this->config->item('error_end_delimiter', 'ion_auth'));
+
+		$this->lang->load('auth');
     }
 
 	public function index()
 	{
-		 $requests = $this->request_model->get();
-		$data['requests'] = $requests;
-		$this->load->view('dashboard', $data);
+		if (!$this->ion_auth->logged_in())
+		{
+			// redirect them to the login page
+			redirect('auth/login', 'refresh');
+		}
+		else if (!$this->ion_auth->is_admin()) // remove this elseif if you want to enable this for non-admins
+		{
+			// redirect them to the home page because they must be an administrator to view this
+			show_error('You must be an administrator to view this page.');
+		}
+		else
+		{
+			$requests = $this->request_model->get();
+			$data['requests'] = $requests;
+			$this->load->view('dashboard', $data);
+		}
 	}
 }
