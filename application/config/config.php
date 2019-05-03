eb43d74167f5c4d14f73e8ed2b1367c7e5373371
Tosin Amuda <tosinamuda@gmail.com>
2019-05-03 09:56:31 +0100

    Still base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,9 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = getenv('BASE_URL');
-echo getenv('BASE_URL');
-exit;
+$config['base_url'] = 'https://upriselive.atomtech.com.ng/';
 
 /*
 |--------------------------------------------------------------------------
