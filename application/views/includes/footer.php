0d74363af9d476fc4d5709e967fbf512cbb957b3
Tosin Amuda <tosinamuda@gmail.com>
2019-05-03 12:29:35 +0100

    Remove JS

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -40,44 +40,6 @@
         <script src='public/js/owl.carousel.min.js'></script>                          
         <script src='public/js/jquery.sticky-kit.min.js'></script>                          
         <script src='public/js/main.js'></script>
-        <script type="text/javascript">
-
-           jQuery("document").ready(function() {
-          // The DOM is ready!
-          // The rest of your code goes here!
-            var form = document.getElementById("submit-form");
-
-            form.onsubmit = function (e) {
-              // stop the regular form submission
-              e.preventDefault();
-
-              // collect the form data while iterating over the inputs
-              var data = {};
-              for (var i = 0, ii = form.length; i < ii; ++i) {
-                var input = form[i];
-                if (input.name) {
-                  data[input.name] = input.value;
-                }
-              }
-
-             
-              jQuery.ajax({
-                type: "POST",
-                url: "https://api.fungifting.com/lead/upriselive",
-                data: data,
-                success: function(data){ 
-                  window.location.href = "/thanks.php"; 
-                },
-                error : function() {console.log("Failed");}
-
-              });
-
-
-              
-            };
-      });
-
-      </script>
 
     </body>
 </html>
