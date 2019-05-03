cc4675ff00b9407c227b78caabb1a92b92a3220e
Tosin Amuda <tosinamuda@gmail.com>
2019-05-03 12:27:01 +0100

    Add Form Helper Libraray

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -6,6 +6,7 @@ class Home extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
+		$this->load->helper('form');
 		$this->load->helper('url');
 		$this->load->library('session');
 		$this->load->library('phpmailer');
