ce8c69cf8748fb62315e4aad4c35ced7f52a1fed
Chamadani Faisal Amri <daniamri93@gmail.com>
2020-03-21 20:48:23 +0700

    Update routes.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'login';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
