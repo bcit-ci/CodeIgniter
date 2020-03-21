ce4ca65456c7dae4c65146907eca19ca2b2963ae
wira-sigalingging <43566126+wira-sigalingging@users.noreply.github.com>
2020-03-21 11:50:40 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('view_login');
+	}
+}
