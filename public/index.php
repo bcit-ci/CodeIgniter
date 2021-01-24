cf65db4a094ce61c93925c6a3b00b35a37e3906b
Mohamed ELIDRISSI <67818913+elidrissidev@users.noreply.github.com>
2021-01-24 18:22:57 +0100

    Update system_path and application_folder variables to point to the right path

--- public/index.php
+++ public/index.php
@@ -97,7 +97,7 @@ switch (ENVIRONMENT)
  * This variable must contain the name of your "system" directory.
  * Set the path if it is not in the same directory as this file.
  */
-	$system_path = 'system';
+	$system_path = '../system';
 
 /*
  *---------------------------------------------------------------
@@ -114,7 +114,7 @@ switch (ENVIRONMENT)
  *
  * NO TRAILING SLASH!
  */
-	$application_folder = 'application';
+	$application_folder = '../application';
 
 /*
  *---------------------------------------------------------------
