d7df8001f91fcce4c91509c45f12e1520ffba2f0
Samsul Arif Zulvian <48172520+kangzoel@users.noreply.github.com>
2021-04-07 21:15:21 +0700

    Add is_exists validation rule

--- system/libraries/Form_validation.php
+++ system/libraries/Form_validation.php
@@ -1132,6 +1132,26 @@ class CI_Form_validation {
 	// --------------------------------------------------------------------
 
 	/**
+	 * Is Exists
+	 *
+	 * Check if the input value exists in the specified
+	 * database field.
+	 *
+	 * @param	string	$str
+	 * @param	string	$field
+	 * @return	bool
+	 */
+	public function is_exists($str, $field)
+	{
+		sscanf($field, '%[^.].%[^.]', $table, $field);
+		return isset($this->CI->db)
+			? ($this->CI->db->limit(1)->get_where($table, array($field => $str))->num_rows() !== 0)
+			: FALSE;
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
 	 * Minimum Length
 	 *
 	 * @param	string
