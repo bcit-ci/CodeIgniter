7a945f4ab59ef58546076355e9ba191b3200c263
doan281 <doan281@gmail.com>
2021-05-21 21:32:45 +0700

    Auto detect protocol and calculate base_url
    Auto detect protocol and calculate base_url in config.php file

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+//$config['base_url'] = '';
+$protocol = (
+	(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') ||
+	(isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
+) ? 'https' : 'http';
+$root = "{$protocol}://" . $_SERVER['HTTP_HOST'];
+$root .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
+$config['base_url'] = $root;
 
 /*
 |--------------------------------------------------------------------------
