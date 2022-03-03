bcb17eb8ba53a85de154439d0ab8ff1bed047bc9
Andrey Andreev <narf@devilix.net>
2022-03-03 15:21:49 +0200

    [ci skip] 3.1.13 release

--- system/core/CodeIgniter.php
+++ system/core/CodeIgniter.php
@@ -56,7 +56,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @var	string
  *
  */
-	const CI_VERSION = '3.1.13-dev';
+	const CI_VERSION = '3.1.13';
 
 /*
  * ------------------------------------------------------
